8f67a1ff52ff142030485bf3faabc7a93cb1caf8
David Hilbert <a.giavaras@gmail.com>
2019-06-06 19:09:10 +0300

    Update predicates and add library primitive types

---
+++ numengine/include/numengine/base/types.h
@@ -0,0 +1,12 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include "parframe/base/types.h"
+namespace numengine
+{
+   /// \brief basic primitive data types
+   using real_t = kernel::real_t;
+   using uint_t = kernel::uint_t;
+}
+
+#endif // TYPES_H
