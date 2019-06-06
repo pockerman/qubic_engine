fb4d005c94acc779e0eaa3beaa33edff7cee7f71
David Hilbert <a.giavaras@gmail.com>
2019-06-06 19:09:36 +0300

    Add first example

---
+++ numengine/examples/exe1/exe.cpp
@@ -0,0 +1,15 @@
+/// \details
+///
+
+
+
+#include "exe.h"
+#include "numengine/geom/mesh.h"
+#include <iostream>
+
+int main(int argc, char** argv) {
+
+    std::cout<<"Running Example"<<std::endl;
+    return 0;
+}
+
---
+++ numengine/examples/exe1/exe.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   exe.h
+ * Author: david
+ *
+ * Created on October 15, 2017, 2:50 PM
+ */
+
+#ifndef EXE_H
+#define	EXE_H
+
+
+namespace exe
+{}
+
+#endif	/* EXE_H */
+
