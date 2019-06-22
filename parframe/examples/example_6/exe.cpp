f02c756d61adc395dfd7f5d3f424a57f725b7b60
David Hilbert <a.giavaras@gmail.com>
2019-06-22 12:15:42 +0300

    Update example_6

--- parframe/examples/example_6/exe.cpp
+++ parframe/examples/example_6/exe.cpp
@@ -9,6 +9,7 @@
 #include "parframe/partitioners/array_partitioner.h"
 #include "parframe/base/algorithm_info.h"
 #include "parframe/data_structs/partitioned_object.h"
+#include "parframe/executors/simple_task.h"
 
 #include <thread>
 #include <vector>
@@ -43,7 +44,51 @@ private:
     uint_t n_itrs_;
     real_t res_;
 
+    struct MatVecProduct;
+
+    std::vector<std::unique_ptr<MatVecProduct>> mat_vec_tasks_;
+
+    // structure responsible for computing the
+    // matrix-vector product
+    struct MatVecProduct: public parframe::SimpleTaskBase
+    {
+
+        MatVecProduct(uint_t id, const Matrix& mat, const Vector& x, Vector& rslt)
+            :
+        parframe::SimpleTaskBase(id),
+        mat_ptr(&mat),
+        x_ptr(&x),
+        rslt_ptr(&rslt)
+        {}
+
+        const Matrix* mat_ptr;
+        const Vector* x_ptr;
+        Vector* rslt_ptr;
+
+        // execute the matrix-vector product
+        virtual void run()override final;
+    };
+
 };
+
+
+void
+CGSolver::MatVecProduct::run(){
+
+    // get the rows partiton indeces corresponding to this task
+    const parframe::range1d<uint_t> parts = mat_ptr->get_partition(this->get_id());
+
+    auto begin = parts.begin();
+    auto end   = parts.end();
+
+    for(uint_t r  = begin; r < end; ++r){
+        auto& rslt = rslt_ptr->operator[](r);
+
+        for(Matrix::ConstIterator it = mat_ptr->begin(r); it != mat_ptr->end(r); ++it )  {
+           rslt += *it*x_ptr->operator[](r);
+        }
+    }
+}
 
 CGSolver::CGSolver(uint_t nitrs, real_t res)
     :
@@ -56,17 +101,30 @@ CGSolver::solve(const Matrix& mat, const Vector& b, Vector& x, ThreadPool& execu
 
     // do basic checks
 
+    parframe::AlgInfo info;
+    info.nthreads = executor.get_n_threads();
+
     // allocate helper vectors
-    Vector g;
-    Vector d;
+    Vector g(x.size());
+    Vector d(x.size());
+    Vector w(x.size());
+
+    mat_vec_tasks_.reserve(executor.get_n_threads());
+
+    // create the tasks
+    for(uint_t t=0; t<executor.get_n_threads(); ++t){
+        mat_vec_tasks_.push_back(std::make_unique<MatVecProduct>(t, mat, x, w));
+    }
 
     for(uint itr = 0; itr < n_itrs_; ++itr){
 
 
+        info.niterations = itr;
 
     }
-}
 
+    return info;
+}
 
 }
 
@@ -75,27 +133,25 @@ int main(){
     try {
 
         using parframe::ThreadPool;
-        Vector x(100, 1.0);
+        Vector x(100, 0.0);
+        Vector b(100, 0);
+        Matrix A(100, 100);
 
         //create a pool and start it with four threads
         ThreadPool pool(4);
 
         parframe::Sum<parframe::real_t> op;
-        std::vector<parframe::range1d<Vector::Iterator>> partitions;
+        std::vector<parframe::range1d<uint_t>> partitions;
 
         //create the partitions
-        partition_range(x.begin(), x.end(), partitions, pool.get_n_threads());
+        uint_t start =0;
+        uint_t end = A.rows();
+        parframe::partition_range(start, end, partitions, pool.get_n_threads());
 
-        parframe::reduce_array(partitions, op, pool);
-        auto rslt = op.get();
+        CGSolver solver(100, parframe::kernel_consts::tolerance());
+        solver.solve(A, x, b, pool);
+        //parframe::reduce_array(partitions, op, pool);
 
-        if( rslt.first != 100.0 && rslt.second == false){
-            std::cout<<"Result: "<<rslt.first<<" should be 100"<<std::endl;
-            throw std::logic_error("Incorrect result");
-        }
-        else{
-            std::cout<<"Result: "<<rslt.first<<" is correct and valid "<<rslt.second<<std::endl;
-        }
     }
     catch (std::logic_error& e) {
         std::cout<<e.what()<<std::endl;
