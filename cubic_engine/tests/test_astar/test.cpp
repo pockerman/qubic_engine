9ca9a3006847e8aa99133aff6ef028fa84157aa0
David Hilbert <a.giavaras@gmail.com>
2019-07-21 18:48:41 +0300

    Correct bug with test enabling

--- cubic_engine/tests/test_astar/test.cpp
+++ cubic_engine/tests/test_astar/test.cpp
@@ -6,14 +6,13 @@
 #include <vector>
 #include <gtest/gtest.h>
 
-//vertex data to apply A*
-
 namespace test_data
 {
     using real_t = cengine::real_t;
     using unit_t = cengine::uint_t;
     using Vec = cengine::DynVec<real_t>;
 
+    //vertex data to apply A*
     struct astar_node
     {
         real_t gcost;
@@ -46,7 +45,6 @@ namespace test_data
     fcost(o.fcost),
     position(o.position)
     {}
-
 }
 
 /**
@@ -72,5 +70,102 @@ TEST(TestAStar, StarAndEndNodesAreTheSame) {
     auto start = path.begin();
     ASSERT_EQ(start->first, static_cast<uint_t>(0));
     ASSERT_EQ(start->second, static_cast<uint_t>(0));
+}
 
+/**
+ * @brief TEST Pathfinding with A*
+ * Scenario: Construct a linear path with 5 vertices that is represented as a graph.
+ * Expected Output: The reconstructed path should simply be 0,1,2,3,4
+ */
+
+TEST(TestAStar, LinearPath) {
+
+    using namespace cengine;
+
+    using test_data::astar_node;
+    kernel::BoostSerialGraph<astar_node, void > graph;
+    DynVec<real_t> vec(3, 0.0);
+
+    graph.add_vertex(std::move(astar_node(vec)));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({1.0, 0.0, 0.0 }))));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({2.0, 0.0, 0.0 }))));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({3.0, 0.0, 0.0 }))));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({4.0, 0.0, 0.0 }))));
+
+    graph.add_edge(0,1);
+    graph.add_edge(1,2);
+    graph.add_edge(2,3);
+    graph.add_edge(3,4);
+
+    kernel::EuclideanMetric metric;
+    std::multimap<uint_t, uint_t> path = astar_search(graph, graph.get_vertex(0), graph.get_vertex(4), metric);
+    std::vector<uint_t> waypoints = cengine::reconstruct_a_star_path(path,graph.get_vertex(4).id);
+
+    ASSERT_EQ(path.empty(), false);
+    ASSERT_EQ(path.size(), static_cast<uint_t>(1));
+    auto start = path.begin();
+    ASSERT_EQ(start->first, static_cast<uint_t>(0));
+    ASSERT_EQ(start->second, static_cast<uint_t>(0));
+
+    ASSERT_EQ(waypoints.empty(), false);
+    ASSERT_EQ(waypoints.size(), 5);
+
+    //the reversed path should be 0->1->2->3->4
+    ASSERT_EQ(waypoints[0], 0);
+    ASSERT_EQ(waypoints[1], 1);
+    ASSERT_EQ(waypoints[2], 2);
+    ASSERT_EQ(waypoints[3], 3);
+    ASSERT_EQ(waypoints[4], 4);
+}
+
+/**
+ * \brief TEST Pathfinding with A*:
+ * Scenario: We construct a  path with 7 vertices that is represented as a graph.
+ * Expected Output: The reconstructed path should simply be 0->2->5->6->7
+ */
+TEST(TestAStar, SearchPath) {
+
+    using namespace cengine;
+
+    using test_data::astar_node;
+    kernel::BoostSerialGraph<astar_node, void > graph;
+    DynVec<real_t> vec(3, 0.0);
+
+    graph.add_vertex(std::move(astar_node(vec)));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({0.5, -0.5, 0.0}))));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({1.0,  1.0,  0.0}))));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({1.5,  -1.0, 0.0}))));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({2.0,   0.0,  0.0}))));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({2.0,   1.0,  0.0}))));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({3.0,   0.5,  0.0}))));
+    graph.add_vertex(std::move(astar_node(DynVec<real_t>({4.0, 0.5,  0.0}))));
+    graph.add_edge(0,1);
+    graph.add_edge(0,2);
+    graph.add_edge(2,4);
+    graph.add_edge(2,5);
+    graph.add_edge(4,6);
+    graph.add_edge(5,6);
+    graph.add_edge(6,7);
+    graph.add_edge(1,3);
+    graph.add_edge(3,7);
+
+    kernel::EuclideanMetric metric;
+    std::multimap<uint_t, uint_t> path = astar_search(graph, graph.get_vertex(0), graph.get_vertex(7), metric);
+    std::vector<uint_t> waypoints = cengine::reconstruct_a_star_path(path,graph.get_vertex(7).id);
+
+    ASSERT_EQ(path.empty(), false);
+    ASSERT_EQ(path.size(), static_cast<uint_t>(5));
+    auto start = path.begin();
+    ASSERT_EQ(start->first, static_cast<uint_t>(0));
+    ASSERT_EQ(start->second, static_cast<uint_t>(0));
+
+    ASSERT_EQ(waypoints.empty(), false);
+    ASSERT_EQ(waypoints.size(), 5);
+
+    //the reversed path should be 0->2->5->6->7
+    ASSERT_EQ(waypoints[0], 0);
+    ASSERT_EQ(waypoints[1], 2);
+    ASSERT_EQ(waypoints[2], 5);
+    ASSERT_EQ(waypoints[3], 6);
+    ASSERT_EQ(waypoints[4], 7);
 }
