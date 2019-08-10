8cd41acf3c89585777f4368a1d325ed59a1d02b0
David Hilbert <a.giavaras@gmail.com>
2019-08-10 16:35:25 +0300

    Add ChronoVehicleWrapper class

---
+++ parframe/src/parframe/vehicles/chrono_vehicle_wrapper.cpp
@@ -0,0 +1,11 @@
+#include "parframe/vehicles/chrono_vehicle_wrapper.h"
+
+namespace kernel
+{
+
+ChronoVehicleWrapper::ChronoVehicleWrapper(const std::string& name)
+    :
+vehicle_(nullptr)
+{}
+
+}
