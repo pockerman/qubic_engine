9d6a2ec25bf42bd21a11c0163f0dc5f9402443dd
David Hilbert <a.giavaras@gmail.com>
2019-09-22 12:32:25 +0300

    #32 Fix serial MC integration example

--- kernel/examples/example_8/exe.cpp
+++ kernel/examples/example_8/exe.cpp
@@ -29,12 +29,13 @@ int main(){
     real_t total_area = 0.0;
     real_t area_under_curve = 0.0;
 
-    std::default_random_engine x_generator;
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd());
+
     std::uniform_real_distribution<real_t> x_distribution(a, b);
 
     auto y_lower = 0.0;
     auto y_upper = f(b);
-    std::default_random_engine y_generator;
     std::uniform_real_distribution<real_t> y_distribution(y_lower, y_upper);
 
     // the area of the rectangle [a,b]x[f(a), f(b)]
@@ -44,8 +45,8 @@ int main(){
     for(uint_t itr=0; itr < N_ITERATIONS; ++itr){
 
         // generate random x and y points
-        real_t x = x_distribution(x_generator);
-        real_t y = y_distribution(y_generator);
+        real_t x = x_distribution(gen);
+        real_t y = y_distribution(gen);
 
         std::cout<<"Generated point: "<<x<<","<<y<<std::endl;
 
@@ -60,7 +61,7 @@ int main(){
     std::cout<<"Rectangle area: "<<RECT_AREA<<std::endl;
     if(area_under_curve != 0.){
 
-        std::cout<<"Total area points: "<<area_under_curve<<std::endl;
+        std::cout<<"Total area points: "<<total_area<<std::endl;
         std::cout<<"Area under curve points: "<<area_under_curve<<std::endl;
         std::cout<<"Calculated area: "<<RECT_AREA*(area_under_curve/total_area)<<std::endl;
     }
