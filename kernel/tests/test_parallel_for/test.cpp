8562a3d78fc9b66cc4cbf282222921a9ca988d66
David Hilbert <a.giavaras@gmail.com>
2019-10-05 12:04:01 +0300

    #47 Debug test for parallel_for

--- kernel/tests/test_parallel_for/test.cpp
+++ kernel/tests/test_parallel_for/test.cpp
@@ -1,7 +1,10 @@
 #include "kernel/parallel/parallel_algos/parallel_for.h"
 #include "kernel/parallel/threading/thread_pool.h"
 #include "kernel/utilities/range_1d.h"
+#include "kernel/parallel/utilities/partitioned_type.h"
+#include "kernel/parallel/utilities/array_partitioner.h"
 #include "kernel/base/types.h"
+#include "kernel/base/exceptions.h"
 #include <vector>
 #include <gtest/gtest.h>
 
@@ -11,15 +14,18 @@ namespace{
 
 }
 
-TEST(TestParallelFor, RunWithEmptyRange) {
+
+TEST(TestParallelFor, RunWithNoPartitions) {
 
     using kernel::uint_t;
     using kernel::ThreadPool;
     using kernel::range1d;
+    using kernel::PartitionedType;
+
 
     /***
-     * Test Scenario:   The application attempts to execute parallel_for on an empty Range 
-     * Expected Output:	parallel_for simply returns silently. If warnings are one a message is printed
+     * Test Scenario:   The application attempts to execute parallel_for with a  Range that is not partitioned
+     * Expected Output:	parallel_for throws InvalidPartitionedObject
      **/
 
     /// TODO: Is there a better way to do this?
@@ -27,18 +33,116 @@ TEST(TestParallelFor, RunWithEmptyRange) {
 
         // this is the executor
         ThreadPool pool(4);
-        range1d<uint_t> range;
+
+        // a dummy partitoned ranges
+        PartitionedType<range1d<uint_t>> range;
 
         // dummy body
         auto body = [](int){return false;};
         kernel::parallel_for(range, body, pool);
 
     }
+    catch(kernel::InvalidPartitionedObject& e){
+        std::string msg = e.what();
+        ASSERT_EQ(msg, "The given range does not have partitions");
+    }
     catch(...){
 
         ASSERT_FALSE("A non expected exception was thrown");
     }
 }
+
+
+TEST(TestParallelFor, RunWithInvalidNumPartitions) {
+
+    using kernel::uint_t;
+    using kernel::ThreadPool;
+    using kernel::range1d;
+    using kernel::PartitionedType;
+
+
+    /***
+     * Test Scenario:   The application attempts to execute parallel_for with a  Range that is not correctly partitioned
+     * Expected Output:	parallel_for throws InvalidPartitionedObject
+     **/
+
+    /// TODO: Is there a better way to do this?
+    std::string expected;
+    try{
+
+        // this is the executor
+        ThreadPool pool(4);
+
+        // a dummy partitoned ranges
+        PartitionedType<range1d<uint_t>> range;
+        std::vector<range1d<uint_t>> partitions(1, range1d<uint_t>());
+        range.set_partitions(partitions);
+
+        expected = "Invalid number of partitions: "+std::to_string(range.n_partitions())+" should be: "+std::to_string(pool.n_processing_elements());
+
+        // dummy body
+        auto body = [](int){return false;};
+        kernel::parallel_for(range, body, pool);
+
+    }
+    catch(kernel::InvalidPartitionedObject& e){
+        std::string msg = e.what();
+
+        ASSERT_EQ(expected, msg);
+    }
+    catch(...){
+
+        ASSERT_FALSE("A non expected exception was thrown");
+    }
+}
+
+
+TEST(TestParallelFor, RunWithIdentityBody) {
+
+    using kernel::uint_t;
+    using kernel::ThreadPool;
+    using kernel::range1d;
+    using kernel::PartitionedType;
+
+
+    /***
+     * Test Scenario:   The application attempts to execute parallel_for with a  Range and a Body
+     * Expected Output:	parallel_for should execute corrctly
+     **/
+
+    /// TODO: Is there a better way to do this?
+    try{
+
+        // this is the executor
+        ThreadPool pool(4);
+
+        std::vector<range1d<uint_t>> partitions;
+        kernel::partition_range(0, 100, partitions, pool.get_n_threads());
+
+        // a dummy partitoned ranges
+        PartitionedType<range1d<uint_t>> range;
+        range.set_partitions(partitions);
+
+        // create the range
+
+        // dummy body
+        auto body = [](uint_t item){return item;};
+        kernel::parallel_for(range, body, pool);
+
+
+
+    }
+    catch(kernel::InvalidPartitionedObject& e){
+        std::string msg = e.what();
+        ASSERT_EQ(msg, "The given range does not have partitions");
+    }
+    catch(...){
+
+        ASSERT_FALSE("A non expected exception was thrown");
+    }
+}
+
+
 
 
 
