c2ea61c4848b76d8bd86a5e5192401ef1b5a7be1
David Hilbert <a.giavaras@gmail.com>
2019-10-06 13:21:11 +0300

    #49 Finish example

--- kernel/examples/example_10/exe.cpp
+++ kernel/examples/example_10/exe.cpp
@@ -1,70 +1,73 @@
 #include "kernel/base/types.h"
 #include "kernel/base/kernel_consts.h"
 #include "kernel/geometry/shapes/circle.h"
+#include "kernel/geometry/geom_point.h"
 
 #include <random>
 #include <iostream>
-
-namespace exe
-{
-using kernel::real_t;
-using kernel::uint_t;
-
-// number of iterations
-const uint_t N_ITERATIONS = 100000;
-
-}
+#include <array>
 
 
 int main(){
 
-    using namespace exe;
+    using kernel::real_t;
+    using kernel::uint_t;
+    using kernel::GeomPoint;
 
-    kernel::Circle circle(2.0);
-
-    // lower and upper limits
-    /*real_t a = 1.0;
-    real_t b = 3.0;
-
-    real_t total_area = 0.0;
-    real_t area_under_curve = 0.0;
+    // number of iterations
+    const uint_t N_ITERATIONS = 100000;
 
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd());
 
-    std::uniform_real_distribution<real_t> x_distribution(a, b);
+    kernel::Circle circle(2.0);
 
-    auto y_lower = 0.0;
-    auto y_upper = f(b);
-    std::uniform_real_distribution<real_t> y_distribution(y_lower, y_upper);
+    real_t x0 = - circle.radius();
+    real_t x1 = circle.radius();
 
-    // the area of the rectangle [a,b]x[f(a), f(b)]
-    const real_t RECT_AREA = (b-a)*(f(b)-0.0);
+    // distribution of the x-coordinate
+    std::uniform_real_distribution<real_t> x_distribution(x0, x1);
 
+    auto y0 = - circle.radius();
+    auto y1 = circle.radius();
+    std::uniform_real_distribution<real_t> y_distribution(y0, y1);
+
+    real_t total_area = 0.0;
+    real_t area_under_curve = 0.0;
+
+    // the area of the rectangle
+    const real_t RECT_AREA = (x1-x0)*(y1-y0);
+
+    // the coordinates of the generated point
+    std::array<real_t, 2> coords;
 
     for(uint_t itr=0; itr < N_ITERATIONS; ++itr){
 
         // generate random x and y points
-        real_t x = x_distribution(gen);
-        real_t y = y_distribution(gen);
+        coords[0] = x_distribution(gen);
+        coords[1] = y_distribution(gen);
 
-        std::cout<<"Generated point: "<<x<<","<<y<<std::endl;
+        GeomPoint<2> point(coords);
+
+        std::cout<<"Generated point: "<<coords[0]<<","<<coords[0]<<std::endl;
 
         // add 1 to count of points within the whole area
         total_area += 1.0;
 
-        if( y  <= f(x)){
+        if( circle.is_inside(point)){
            area_under_curve += 1;
         }
     }
 
     std::cout<<"Rectangle area: "<<RECT_AREA<<std::endl;
+    std::cout<<"Total area points: "<<total_area<<std::endl;
+
     if(area_under_curve != 0.){
 
-        std::cout<<"Total area points: "<<total_area<<std::endl;
         std::cout<<"Area under curve points: "<<area_under_curve<<std::endl;
         std::cout<<"Calculated area: "<<RECT_AREA*(area_under_curve/total_area)<<std::endl;
-    }*/
+        std::cout<<"Circle area: "<<circle.area()<<std::endl;
+    }
 
     return 0;
 }
