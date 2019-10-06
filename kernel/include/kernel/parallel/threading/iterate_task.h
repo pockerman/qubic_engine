981c5fccbd42d1d18eabb41307a3b1b79d893c97
David Hilbert <a.giavaras@gmail.com>
2019-10-06 12:50:17 +0300

    #47 Delete commented out code

--- kernel/include/kernel/parallel/threading/iterate_task.h
+++ kernel/include/kernel/parallel/threading/iterate_task.h
@@ -76,64 +76,6 @@ IterateTask<RangeType, BodyType, ValueType>::run(){
     }
 }
 
-
-/*
-template<typename IteratorTp, typename OpTp>
-class IterateTaskWithResult
-{
-
-public:
-
-    typedef IteratorTp iterator_t;
-    typedef OpTp operation_t;
-    typedef typename operation_t::result_t result_t;
-
-    /// \brief Default constructor. No work is done
-    /// if  a task is created via this function and
-    /// not manipulated appropriately by the public API
-    /// of this clas
-    IterateTaskWithResult();
-
-    /// \brief Constructor. Initialize with the range
-    /// the task is meant to work on and any arguments the
-    /// operation requires to execute
-    template<typename...Args>
-    IterateTaskWithResult(const kernel::range1d<iterator_t>& range, Args&... args);
-
-    /// \brief Execute the task
-    void operator()(){task_();}
-
-    /// \brief Set the range to work on
-    void set_range(const kernel::range1d<iterator_t>& range){task_.set_range(range);}
-
-    /// \brief Set the operation data
-    void set_op(const operation_t& op){ task_.set_op(op);}
-
-    /// \brief Get the result of applying the operation over the
-    /// given iterator range
-    result_t get_result()const{return task_.op_.get_result();}
-
-private:
-
-    IterateTask<iterator_t, operation_t> task_;
-
-};
-
-template<typename IteratorTp, typename OpTp>
-IterateTaskWithResult<IteratorTp, OpTp>::IterateTaskWithResult()
-    :
-   task_()
-{}
-
-template<typename IteratorTp, typename OpTp>
-template<typename...Args>
-IterateTaskWithResult<IteratorTp, OpTp>::IterateTaskWithResult(const kernel::range1d<iterator_t>& range, Args&... args)
-    :
- task_(range, args...)
-{}
-
-*/
-
 }
 
 #endif // ITERATE_TASK_H
