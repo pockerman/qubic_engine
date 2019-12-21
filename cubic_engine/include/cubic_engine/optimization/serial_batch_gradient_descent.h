dbd16fdd0f1ee10620eb14bba3ba211d9481a629
Alex Giavaras <a.giavaras@gmail.com>
2019-12-21 13:46:20 +0200

    #77 Add batch gradient descent algorithm

--- cubic_engine/include/cubic_engine/optimization/serial_batch_gradient_descent.h
+++ cubic_engine/include/cubic_engine/optimization/serial_batch_gradient_descent.h
@@ -1,37 +1,54 @@
-/* 
- * File:   gradient_descent.h
- * Author: david
- *
- * Created on June 20, 2016, 1:20 PM
- */
-
 #ifndef SERIAL_BATCH_GRADIENT_DESCENT_H
 #define	SERIAL_BATCH_GRADIENT_DESCENT_H
 
-#include "parml/base/parml_config.h"
-#include "parml/base/parml.h"
-#include "parml/algorithms/gradient_descent_info_control.h"
-#include "parml/algorithms/gradient_descent_type.h"
-
-#ifdef PARML_DEBUG
-#include "utilities/exceptions.h"
-#endif 
+#include "cubic_engine/base/cubic_engine_types.h"
+#include "kernel/base/iteration_control.h"
+#include "kernel/utilities/iterative_algorithm_controller.h"
+#include "kernel/base/algorithm_info.h"
+#include "kernel/base/kernel_consts.h"
 
 #include <chrono>
 #include <iostream>
 #include <vector>
 
-namespace parml
+namespace cengine
 {
         
 /**
  * @brief Implementation of the gradient descent (GC) algorithm
  * for solving optimization problems. There are two implementations
  * of the algorithm. Batch gradient descent and stochastic gradient descent.
- * This is the serial batched version of the algorithm
  */
-template<>
-class Gd<gradient_descent_type::Type::SERIAL_BGD> 
+
+struct GDControl: public kernel::IterativeAlgorithmController
+{
+    constexpr static real_t DEFAULT_LEARNING_RATE = 0.01;
+
+    /// \brief The learning rate
+    real_t learning_rate;
+
+    /// \brief Flag indicating if iteration infor should be printed
+    bool show_iterations;
+
+    GDControl( uint_t max_num_itrs, real_t tolerance=kernel::KernelConsts::tolerance(), real_t eta=GDControl::DEFAULT_LEARNING_RATE );
+
+};
+
+inline
+GDControl::GDControl( uint_t max_num_itrs, real_t tolerance, real_t eta_ )
+    :
+kernel::IterativeAlgorithmController(max_num_itrs,  tolerance),
+learning_rate(eta_)
+{}
+
+struct GDInfo: public kernel::AlgInfo
+{
+
+    real_t learning_rate;
+
+};
+
+class Gd
 {
     
 public:
@@ -45,7 +62,7 @@ public:
     /**
      * @brief Constructor
      */
-    Gd(const GDControl& data);
+    Gd(const GDControl& input);
     
     /**
      * @brief Solves the optimization problem. Returns information
@@ -53,74 +70,71 @@ public:
      */
     template<typename MatType,typename VecType,typename ErrorFuncType,typename HypothesisFuncType>
     GDInfo solve(const MatType& mat,const VecType& v, 
-                 const ErrorFuncType& fun,HypothesisFuncType& h)const;
-    
-    
-    template<typename MatType,typename VecType,typename ErrorFuncType,typename HypothesisFuncType>
-    GDInfo solve(const MatType* mat,const VecType* v, 
-                 const ErrorFuncType& fun,HypothesisFuncType& h)const{
-        
-        return solve(*mat,*v,fun,h);
-    }
-    
-    
+                 const ErrorFuncType& fun, HypothesisFuncType& h);
+      
 private:
     
     /**
      * @brief The data the GD solver is using
      */
-    const GDControl data_;
+    GDControl input_;
     
 };
 
+inline
+Gd::Gd(const GDControl& input)
+    :
+      input_(input)
+{}
 
 template<typename MatType,typename VecType,typename ErrorFuncType,typename HypothesisFuncType>
 GDInfo 
-Gd<gradient_descent_type::Type::SERIAL_BGD>::solve(const MatType& mat,const VecType& v, 
-                           const ErrorFuncType& error_fun,HypothesisFuncType& h)const{
+Gd::solve(const MatType& mat,const VecType& v,
+                        const ErrorFuncType& error_fun,HypothesisFuncType& h){
     
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
     
     //the info object to return
     GDInfo info;
-    info.learning_rate = data_.learning_rate;
-    info.type = gradient_descent_type::Type::SERIAL_BGD;
+    info.learning_rate = input_.learning_rate;
+
     
     //this should be scaled
-    real_type Jold = error_fun.value(h); 
-    real_type Jcur = utilities::scalar_value_traits<real_type>::zero();
+    real_t Jold = error_fun.value(h);
+    real_t Jcur = 0.0;
     
-    const size_type ncoeffs = h.n_coeffs();
+    const size_t ncoeffs = h.n_coeffs();
     
     //the gradients of the error function.
-    std::vector<real_type> Jgrads(ncoeffs,utilities::scalar_value_traits<real_type>::zero());
+    std::vector<real_t> Jgrads(ncoeffs, 0.0);
     
-    size_type niterations = data_.max_n_iterations; 
-    
-    //start the iterations
-    for(size_type itr=1; itr<=niterations; ++itr){
-       
+    //uint_t niterations = data_.max_n_iterations;
+
+    while(input_.continue_iterations()){
+
         //calculate the gradients
         error_fun.gradients(Jgrads,h);
         
         //update the coefficients
-        std::vector<real_type>& coeffs = h.coeffs();
+        std::vector<real_t>& coeffs = h.coeffs();
         
-        for(size_type c=0; c<ncoeffs; ++c){
-            coeffs[c] -= data_.learning_rate*Jgrads[c];
+        for(uint_t c=0; c<ncoeffs; ++c){
+            coeffs[c] -= input_.learning_rate*Jgrads[c];
         }
         
         //recalculate...this is already scaled
         Jcur = error_fun.value(h);
-        real_type error = std::fabs(Jcur-Jold);
+        real_t error = std::fabs(Jcur-Jold);
+
+        uint_t itr = input_.get_current_tteration();
         
-        if(data_.show_iterations){
+        if(input_.show_iterations){
             
             std::cout<<"BatchGD: iteration: "<<itr<<std::endl;
             std::cout<<"\tJold: "<<Jold<<" Jcur: "<<Jcur
                      <<" error std::fabs(Jcur-Jold): "<<error
-                     <<" exit tolerance: "<<data_.tolerance<<std::endl;
+                     <<" exit tolerance: "<<input_.get_exit_tolerance()<<std::endl;
         }
         
         if(error < data_.tolerance){
@@ -134,15 +148,16 @@ Gd<gradient_descent_type::Type::SERIAL_BGD>::solve(const MatType& mat,const VecT
         Jold = Jcur;
         
         //zero the gradients
-        for(size_type c=0; c<Jgrads.size(); ++c){
+        for(uint_t c=0; c<Jgrads.size(); ++c){
             
-            Jgrads[c] = utilities::scalar_value_traits<real_type>::zero();
+            Jgrads[c] = 0.0;
         }
            
     }//itrs
     
-    if(!info.converged)
-        info.niterations = data_.max_n_iterations;
+    if(!info.converged){
+        info.niterations = input_.get_max_iterations();
+    }
     
     end = std::chrono::system_clock::now();
     info.runtime = end-start;
