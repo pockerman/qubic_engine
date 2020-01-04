c4d52bcb9e6a056530d9de54df94ecff5e4d0684
Alex Giavaras <a.giavaras@gmail.com>
2020-01-04 14:38:53 +0200

    #91 Add example for logistic regression

--- cubic_engine/examples/exe12/exe.cpp
+++ cubic_engine/examples/exe12/exe.cpp
@@ -1,63 +1,67 @@
-#include "kernel/base/config.h"
-
-#ifdef USE_OPENMP
-#include "kernel/parallel/threading/openmp_executor.h"
-#endif
-
-#include "kernel/base/kernel_consts.h"
-#include "kernel/parallel/threading/thread_pool.h"
-#include "kernel/maths/functions/real_vector_polynomial.h"
-#include "kernel/maths/errorfunctions/mse_function.h"
-#include "kernel/utilities/data_set_loaders.h"
 
 #include "cubic_engine/base/cubic_engine_types.h"
-#include "cubic_engine/optimization/threaded_batch_gradient_descent.h"
+#include "cubic_engine/ml/supervised_learning/logistic_regression.h"
+#include "cubic_engine/optimization/serial_batch_gradient_descent.h"
 #include "cubic_engine/optimization/utils/gd_control.h"
+
+#include "kernel/maths/functions/real_vector_polynomial.h"
+#include "kernel/maths/errorfunctions/mse_function.h"
+#include "kernel/maths/functions/sigmoid_function.h"
+#include "kernel/utilities/data_set_loaders.h"
 
 #include <iostream>
 
-       
 int main(){
 
     using cengine::uint_t;
     using cengine::real_t;
     using cengine::DynMat;
     using cengine::DynVec;
-    using cengine::ThreadedGd;
     using cengine::GDControl;
+    using cengine::Gd;
+    using cengine::LogisticRegression;
     using kernel::RealVectorPolynomialFunction;
     using kernel::MSEFunction;
-    using kernel::PartitionedType;
-    using kernel::LogisticRegressor;
+    using kernel::SigmoidFunction;
 
-    const uint_t NUM_THREADS = 2;
+    try{
 
-    auto dataset = kernel::load_reduced_iris_with_partitions(NUM_THREADS);
+        auto dataset = kernel::load_reduced_iris_data_set();
 
-    kernel::ThreadPool executor(NUM_THREADS);
+        // the classifier to use. use a hypothesis of the form
+        // f = w_0 + w_1*x_1 + w_2*x_2 + w_3*x_3 + w_4*x_4;
+        // set initial weights to 0
+        LogisticRegression<RealVectorPolynomialFunction,
+                          SigmoidFunction<RealVectorPolynomialFunction>> classifier({0.0, 0.0, 0.0, 0.0, 0.0});
 
-    //hypothesis of the form f = w_0 + w_1*x
-    // where w_0 = w_1 = 0.0
-    RealVectorPolynomialFunction hypothesis({0.0, 0.0});
+        SigmoidFunction<RealVectorPolynomialFunction> sigmoid_h(classifier.get_model());
 
-    // the error functionto to use for measuring the error
-    MSEFunction<RealVectorPolynomialFunction,
-                PartitionedType<DynMat<real_t>>,
-                PartitionedType<DynVec<real_t>>> mse(hypothesis);
+        // the error function to to use for measuring the error
+        MSEFunction<SigmoidFunction<RealVectorPolynomialFunction>,
+                    DynMat<real_t>,
+                    DynVec<uint_t>> mse(sigmoid_h);
 
-    // the classifier to use
-    LogisticRegressor<SigmoidTransformer> classifier(hypothesis);
+        GDControl control(10000, kernel::KernelConsts::tolerance(), GDControl::DEFAULT_LEARNING_RATE);
+        control.show_iterations = false;
+        Gd gd(control);
 
-    GDControl control(10000, kernel::KernelConsts::tolerance(), GDControl::DEFAULT_LEARNING_RATE);
-    control.show_iterations = false;
+        auto result = classifier.train(dataset.first, dataset.second, gd, mse);
+        std::cout<<result<<std::endl;
 
-    ThreadedGd gd(control);
+        DynVec<real_t> point{1.0, 5.7, 2.8, 4.1, 1.3};
+        auto class_idx = classifier.predict(point);
 
-    auto result = gd.solve(dataset.first, dataset.second, mse, hypothesis, executor, kernel::Null());
-    std::cout<<result<<std::endl;
-    std::cout<<"Intercept: "<<hypothesis.coeff(0)<<", slope: "<<hypothesis.coeff(1)<<std::endl;
+        std::cout<<"Class index: "<<class_idx<<std::endl;
+    }
+    catch(std::exception& e){
 
-        
+        std::cerr<<e.what()<<std::endl;
+    }
+    catch(...){
+
+        std::cerr<<"Unknown exception occured"<<std::endl;
+    }
+
     return 0;
 }
 
