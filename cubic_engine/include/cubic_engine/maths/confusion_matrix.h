48459370a81b796b8412c49ab358e096d3277dcd
Alex Giavaras <a.giavaras@gmail.com>
2020-01-10 10:17:58 +0200

    #97 Add ConfusionMatrix

--- cubic_engine/include/cubic_engine/maths/confusion_matrix.h
+++ cubic_engine/include/cubic_engine/maths/confusion_matrix.h
@@ -2,8 +2,11 @@
 #define CONFUSION_MATRIX_H
 
 #include "cubic_engine/base/cubic_engine_types.h"
+
 #include <map>
 #include <vector>
+#include <exception>
+#include <ostream>
 
 namespace cengine
 {
@@ -11,26 +14,49 @@ namespace cengine
 /// \brief ConfusionMatrix. Wraps the idea of a confusion matrix
 /// A confusion matrix can be used to evaluate the accuracy of a classification.
 /// see https://en.wikipedia.org/wiki/Confusion_matrix
+/// The format of the matrix is the following
+/// actual classes are aligned row-wise
+/// predicted classes are aligned column-wise
 class ConfusionMatrix
 {
 public:
 
     /// \brief Costructor
-    ConfusionMatrix(const std::map<uint_t, std::vector<uint_t>>& data);
+    template<typename Container>
+    ConfusionMatrix(const Container& actual, const Container& predicted, uint_t n_classes);
 
     /// \brief Construct the mtrix from the given map
-    void build_from(const std::map<uint_t, std::vector<uint_t>>& data);
+    template<typename Container>
+    void build_from(const Container& actual, const Container& predicted, uint_t n_classes);
 
-    /// \brief recall also known as sensitivity
-    real_t recall()const;
+    /// \brief Returns the accuracy of the classifier
+    real_t accuracy()const;
 
-    /// \brief Return the  F1-score
-    real_t f1_score()const;
+    /// \brief Returns the miclassification rate
+    real_t misclassification_rate()const;
+
+    /// \brief Recall also known as sensitivity or true positive rate
+    /// for class c
+    real_t recall_class(uint_t c)const;
 
     /// \breif Return the true positives. This is simply
     /// the sum of the diagonal elements
     uint_t true_positives()const;
 
+    /// \brief Get the class counts
+    uint_t get_class_counts(uint_t c)const;
+
+    /// \brief Get class incorrect counts
+    uint_t get_class_incorrect_counts(uint_t c)const;
+
+    /// \brief Returns the counts that class c was predicted as other class
+    uint_t get_class_counts_as_other_class(u_int c, uint_t other)const;
+
+    /// \brief Returns the total number of observations
+    uint_t total_count()const{return total_count_;}
+
+    /// \brief Print the matrix
+    std::ostream& print(std::ostream& out)const;
 
 private:
 
@@ -38,7 +64,75 @@ private:
     /// \brief The matrix that holds the predictions
     DynMat<uint_t> matrix_;
 
+    /// \brief Total number of observations
+    uint_t total_count_;
+
 };
+
+template<typename Container>
+ConfusionMatrix::ConfusionMatrix(const Container& actual,
+                                 const Container& predicted, uint_t n_classes)
+    :
+    matrix_(),
+    total_count_(0)
+{
+    build_from(actual, predicted, n_classes);
+}
+
+template<typename Container>
+void
+ConfusionMatrix::build_from(const Container& actual, const Container& predicted, uint_t n_classes){
+
+    if(n_classes <= 1){
+
+       throw std::invalid_argument("Invalid number of classes: "+
+                                   std::to_string(n_classes)+
+                                   " number of classes should be at least 2");
+    }
+
+    if(actual.size() == 0){
+        throw std::invalid_argument("Cannot construct ConfusionMatrix from empty data");
+    }
+
+    if(actual.size() != predicted.size()){
+        throw std::invalid_argument("A ConfusionMatrix is a square matrix but the given data are: "+
+                                    std::to_string(actual.size())+
+                                    " and columns: "+
+                                    std::to_string(predicted.size()));
+    }
+
+    matrix_.resize(n_classes, n_classes);
+
+    for(uint_t r=0; r<matrix_.rows(); ++r){
+
+        for(uint_t c=0; c<matrix_.rows(); ++c){
+            matrix_(r,c) = 0;
+        }
+    }
+
+    // construct the matrix row-wise
+    for(uint_t r=0; r<actual.size(); ++r){
+
+        uint_t row_idx = actual[r];
+
+        if(actual[r] == predicted[r]){
+           matrix_(row_idx ,row_idx) += 1;
+        }
+        else{
+           matrix_(row_idx , predicted[r]) += 1;
+        }
+    }
+
+    // set the total count of observations
+    total_count_ = actual.size();
+
+}
+
+inline
+std::ostream& operator<<(std::ostream& out, const ConfusionMatrix& mat){
+    return mat.print(out);
+}
+
 
 }
 #endif // CONFUSION_MATRIX_H
--- cubic_engine/source/cubic_engine/maths/confusion_matrix.cpp
+++ cubic_engine/source/cubic_engine/maths/confusion_matrix.cpp
@@ -1,64 +1,124 @@
 #include "cubic_engine/maths/confusion_matrix.h"
 #include <exception>
+
 namespace cengine
 {
 
+real_t
+ConfusionMatrix::accuracy()const{
 
-ConfusionMatrix::ConfusionMatrix(const std::map<uint_t, std::vector<uint_t>>& data)
-    :
-      matrix_()
-{
-    build_from(data);
+    return true_positives()/total_count();
 }
 
-
-void
-ConfusionMatrix::build_from(const std::map<uint_t, std::vector<uint_t>>& data){
-
-    if(data.empty()){
-        throw std::invalid_argument("Cannot construct ConfusionMatrix from empty data");
-    }
-
-    uint_t rows = data.size();
-    uint_t cols = data.begin()->second.size();
-
-    if(rows != cols){
-        throw std::invalid_argument("A ConfusionMatrix is a square matrix but rows are: "+
-                                    std::to_string(rows)+
-                                    " and columns: "+
-                                    std::to_string(cols));
-    }
-
-     matrix_.resize(rows, cols);
-     auto itr = data.begin();
-
-     for(uint_t r=0; r<rows; ++r){
-
-         const auto& row = itr->second; //data[r];
-         for(uint_t c=0; c<cols; ++c){
-
-            matrix_(r, c) = row[c];
-         }
-
-         ++itr;
-     }
+real_t
+ConfusionMatrix::misclassification_rate()const{
+    return 1.0 - accuracy();
 }
-
 
 uint_t
 ConfusionMatrix::true_positives()const{
-    return 0;
+
+    uint_t result = 0;
+    for(uint_t r=0; r<matrix_.rows(); ++r){
+
+        result += get_class_counts(r);
+    }
+    return result;
 }
 
 real_t
-ConfusionMatrix::recall()const{
+ConfusionMatrix::recall_class(uint_t c)const{
+
+    // how many times did we predicted the class
+    uint_t class_counts = get_class_counts(c);
+
+    // how many times we missed it overall
+    uint class_incorrect_counts = get_class_incorrect_counts(c);
+
+    return class_counts/ (class_incorrect_counts + class_incorrect_counts);
+}
+
+uint_t
+ConfusionMatrix::get_class_counts(uint_t c)const{
+
+    if( c >= matrix_.rows()){
+        throw std::invalid_argument("Invlaid class index. Index "+
+                                    std::to_string(c)+
+                                    " not in [0, "+
+                                    std::to_string(matrix_.rows())+
+                                    ")");
+    }
+
+    return matrix_(c ,c);
+}
+
+uint_t
+ConfusionMatrix::get_class_counts_as_other_class(u_int c, uint_t other)const{
+
+    if( c >= matrix_.rows()){
+        throw std::invalid_argument("Invlaid class index. Index "+
+                                    std::to_string(c)+
+                                    " not in [0, "+
+                                    std::to_string(matrix_.rows())+
+                                    ")");
+    }
+
+    if( other >= matrix_.rows()){
+        throw std::invalid_argument("Invlaid class index. Index "+
+                                    std::to_string(other)+
+                                    " not in [0, "+
+                                    std::to_string(matrix_.rows())+
+                                    ")");
+    }
+
+    return matrix_(c, other);
 
 }
 
+uint_t
+ConfusionMatrix::get_class_incorrect_counts(uint_t c)const{
 
-real_t
-ConfusionMatrix::f1_score()const{
+    if( c >= matrix_.rows()){
+        throw std::invalid_argument("Invlaid class index. Index "+
+                                    std::to_string(c)+
+                                    " not in [0, "+
+                                    std::to_string(matrix_.rows())+
+                                    ")");
+    }
+
+    uint_t result = 0;
+
+    for(uint_t r = 0; r<matrix_.rows(); ++r){
+
+        if( r != c){
+
+            result +=  matrix_(c , r);
+        }
+    }
+
+    return result;
 
 }
 
+std::ostream&
+ConfusionMatrix::print(std::ostream& out)const{
+
+    for(uint_t r = 0; r<matrix_.rows(); ++r){
+
+        for(uint_t c = 0; c<matrix_.rows(); ++c){
+
+            out << matrix_(r, c);
+
+            if(c != matrix_.rows() -1){
+                out<<" , ";
+            }
+            else{
+                out<<"\n";
+            }
+        }
+        out<<"\n";
+    }
+
+    return out;
+}
 }
