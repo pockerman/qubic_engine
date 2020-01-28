96ec2cfde712f73b68579a1e738c5b6d4c5c9b12
Alex Giavaras <a.giavaras@gmail.com>
2020-01-28 22:46:47 +0200

    #181 Update example

--- cubic_engine/examples/exe19/exe.cpp
+++ cubic_engine/examples/exe19/exe.cpp
@@ -1,12 +1,14 @@
 #include "cubic_engine/base/cubic_engine_types.h"
-#include "cubic_engine/ml/instance_learning/serial_knn.h"
+#include "cubic_engine/ml/instance_learning/threaded_knn.h"
 #include "cubic_engine/ml/instance_learning/utils/knn_control.h"
 #include "cubic_engine/ml/instance_learning/utils/knn_classification_policy.h"
 #include "cubic_engine/ml/instance_learning/utils/knn_info.h"
+
 #include "kernel/utilities/data_set_loaders.h"
 #include "kernel/maths/lp_metric.h"
 #include "kernel/utilities/csv_file_writer.h"
-
+#include "kernel/parallel/utilities/partitioned_type.h"
+#include "kernel/parallel/threading/thread_pool.h"
 #include <iostream>
 
 
@@ -18,23 +20,31 @@ int main(){
     using cengine::DynVec;
     using cengine::KnnControl;
     using cengine::KnnClassificationPolicy;
-    using cengine::Knn;
+    using cengine::ThreadedKnn;
+    using cengine::Null;
     using kernel::LpMetric;
     using kernel::CSVWriter;
+    using kernel::PartitionedType;
+    using kernel::ThreadPool;
 
     typedef LpMetric<2> similarity_t;
     typedef KnnClassificationPolicy actor_t;
 
     try{
 
-        auto dataset = kernel::load_iris_data_set(false);
+        const uint_t N_THREADS = 4;
+        ThreadPool executor(N_THREADS);
 
-        Knn<DynMat<real_t>, DynVec<uint_t>, similarity_t, actor_t> knn(KnnControl(4));
+        auto dataset = kernel::load_iris_data_set_with_partitions(4, false);
+
+        ThreadedKnn<PartitionedType<DynMat<real_t>>,
+                    PartitionedType<DynVec<uint_t>>, similarity_t, actor_t> knn(KnnControl(4));
+
         knn.train(dataset.first, dataset.second);
-        auto result = knn.predict(dataset.first);
-        std::cout<<result.second<<std::endl;
+        //auto result = knn.predict(dataset.first, executor, Null());
+        //std::cout<<result.second<<std::endl;
 
-        auto& predictions = result.first;
+        /*auto& predictions = result.first;
 
         {
             kernel::CSVWriter writer("knn_classifier_" + std::to_string(result.second.n_neighbors),
@@ -64,14 +74,14 @@ int main(){
 
                 writer.write_row(data_row);
             }
-        }
+        }*/
 
         {
-            kernel::CSVWriter writer("knn_classifier_exact" ,
+            kernel::CSVWriter writer("iri_data_set_parition" ,
                                      kernel::CSVWriter::default_delimiter(), true);
 
             std::vector<std::string> names(dataset.first.columns() + 1);
-            names[0] = "ClusterId";
+            names[0] = "PartitionId";
 
             for(uint_t i=1; i<names.size(); ++i){
                 names[i] = "X-"+std::to_string(i);
@@ -82,17 +92,23 @@ int main(){
 
             std::vector<real_t> data_row(names.size());
 
-            for(uint_t i=0; i<dataset.second.size(); ++i){
+            uint_t n_partitions = dataset.first.n_partitions();
 
-                auto class_idx = dataset.second[i];
-                data_row[0] = class_idx;
-                auto row = kernel::get_row(dataset.first, i);
+            for(uint_t partition=0; partition < n_partitions; ++partition){
 
-                for(uint_t r=0; r<row.size(); ++r){
-                    data_row[r+1] = row[r];
+                data_row[0] = partition;
+                auto range = dataset.first.get_partition(partition);
+
+                for(uint_t start=range.begin(); start<range.end(); ++start){
+
+                    auto row = kernel::get_row(dataset.first, start);
+
+                    for(uint_t r=0; r<row.size(); ++r){
+                        data_row[r+1] = row[r];
+                    }
+
+                    writer.write_row(data_row);
                 }
-
-                writer.write_row(data_row);
             }
         }
     }
