a953ed14443aab72a548956363ccafcd2f530325
Alex Giavaras <a.giavaras@gmail.com>
2020-02-08 22:55:27 +0200

    #86 Improve documentation

--- kernel/include/kernel/utilities/algorithm_info.h
+++ kernel/include/kernel/utilities/algorithm_info.h
@@ -1,14 +1,7 @@
-/* 
- * File:   algorithm_info.h
- * Author: david
- *
- * Created on July 28, 2016, 9:49 AM
- */
-
 #ifndef ALGORITHM_INFO_H
 #define	ALGORITHM_INFO_H
 
-#include "kernel/base/types.h" //application types
+#include "kernel/base/types.h"
 #include "kernel/base/kernel_consts.h"
 
 #include <chrono>
@@ -18,71 +11,67 @@
 namespace kernel
 {
  
-	/**
-	 * @brief A small struct that holds basic information
-	 * about the performance of an algorithm.
-	 */
-	struct AlgInfo
-	{
-        /**
-         *@brief How many iterations the algorithm performed
-         */
-        uint_t niterations;
+/**
+ * @brief A small struct that holds basic information
+ * about the performance of an algorithm.
+ */
+struct AlgInfo
+{
+    /**
+     *@brief How many iterations the algorithm performed
+     */
+    uint_t niterations;
 
-        /**
-         * @brief The residual after the algorithm terminated
-         */
-        real_t residual;
+    /**
+     * @brief The residual after the algorithm terminated
+     */
+    real_t residual;
 
-        /**
-         * @brief Flag indicating if the algorithm converged
-         */
-        bool converged;
+    /**
+     * @brief Flag indicating if the algorithm converged
+     */
+    bool converged;
 
-        /// \brief The exit tolerace used
-        real_t tolerance;
+    /// \brief The exit tolerace used
+    real_t tolerance;
 
-        /**
-         * @brief The total time duration of the algorithm
-         */
-        std::chrono::duration<real_t> runtime;
+    /**
+     * @brief The total time duration of the algorithm
+     */
+    std::chrono::duration<real_t> runtime;
 
-         /**
-          * @brief How many threads the algorithm used
-          */
-         uint_t nthreads;
+     /**
+      * @brief How many threads the algorithm used
+      */
+     uint_t nthreads;
 
-         /**
-          * @brief How many processors the algorithm used
-          */
-         uint_t nprocs;
+     /**
+      * @brief How many processors the algorithm used
+      */
+     uint_t nprocs;
 
-         /**
-          * @brief Constructor
-          */
-		  AlgInfo()
-		          :
-		         niterations(KernelConsts::invalid_size_type()),
-                 residual(std::numeric_limits<real_t>::max()),
-		         converged(false),
-		         runtime(),
-		         nthreads(1),
-		         nprocs(1)
-		  {}
-		  
-		  
-		  /**
-		   * @brief Print the information about the performance of
-		   * the algorithm on the given stream
-		   */
-		  virtual std::ostream& print(std::ostream& out)const;   
-	};
+     /**
+      * @brief Constructor
+      */
+      AlgInfo()
+              :
+             niterations(KernelConsts::invalid_size_type()),
+             residual(std::numeric_limits<real_t>::max()),
+             converged(false),
+             runtime(),
+             nthreads(1),
+             nprocs(1)
+      {}
 
-	inline
-	std::ostream& operator<<(std::ostream& out,const AlgInfo& info ){
-		  
-		  return info.print(out);
-	}
+      /// \brief Print the information about the performance of
+      /// the algorithm on the given stream
+      virtual std::ostream& print(std::ostream& out)const;
+};
+
+inline
+std::ostream& operator<<(std::ostream& out,const AlgInfo& info ){
+    return info.print(out);
+}
 		    
 }
 
