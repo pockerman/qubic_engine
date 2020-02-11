10875fb1f01fcaa2d04475561bba808c56f2feed
Alex Giavaras <a.giavaras@gmail.com>
2020-02-11 10:45:25 +0200

    #199 Fix bugs and add new API methods

--- kernel/src/kernel/numerics/fv_interpolation_factory.cpp
+++ kernel/src/kernel/numerics/fv_interpolation_factory.cpp
@@ -1,4 +1,8 @@
 #include "kernel/numerics/fv_interpolation_factory.h"
+#include "kernel/numerics/fv_ud_interpolation.h"
+#include "kernel/numerics/fv_linear_interpolation.h"
+
+#include <exception>
 
 namespace kernel{
 namespace numerics{
@@ -8,6 +12,19 @@ template<int dim>
 std::shared_ptr<FVInterpolateBase<dim>>
 FVInterpolationFactory<dim>::build(FVInterpolationType type){
 
+    std::shared_ptr<FVInterpolateBase<dim>> ptr;
+
+    switch (type) {
+    case FVInterpolationType::UD:
+        ptr.reset(new FVUDInterpolate<dim>());
+    case FVInterpolationType::LINEAR:
+        ptr.reset(new FVLinearInterpolate<dim>());
+    default:
+        throw std::logic_error("Invalid FVInterpolationType type");
+    }
+
+
+    return ptr;
 }
 
 }
--- kernel/src/kernel/numerics/fv_ud_interpolation.cpp
+++ kernel/src/kernel/numerics/fv_ud_interpolation.cpp
@@ -1,5 +1,9 @@
 #include "kernel/numerics/fv_ud_interpolation.h"
+#include "kernel/discretization/element.h"
+#include "kernel/discretization/face_element.h"
+
 #include <exception>
+
 namespace kernel{
 namespace numerics{
 
@@ -28,6 +32,67 @@ FVUDInterpolate<dim>::compute_fluxes(const Element<dim>& elem, std::vector<real_
 
     if(!velocity_){
         throw std::logic_error("Velocity pointer is NULL");
+    }
+}
+
+template<int dim>
+void
+FVUDInterpolate<dim>::compute_matrix_contributions(const Element<dim>& element,
+                                                   std::vector<real_t>& values)const{
+
+    if(!velocity_){
+        throw std::logic_error("Velocity pointer is NULL");
+    }
+
+    {
+        std::vector<real_t> zero(values.size(), 0.0);
+        values.swap(zero);
+    }
+
+    // loop over the element faces
+    for(uint_t f=0; f<element.n_faces(); ++f){
+
+        auto& face = element.get_face(f);
+
+        if(!face.on_boundary()){
+
+            auto flux = compute_flux(face);
+
+            if(face.is_owner(element.get_id())){
+
+                if(flux >= 0.0){
+
+                    // the contribution goes to the
+                    // diagonal
+                    values[0] += flux;
+                }
+                else{
+                    // it goes to the off diagonal
+                    // which neighbor this is
+                    auto& neighbor = face.get_neighbor();
+                    auto idx = element.which_neighbor_am_i(neighbor);
+                    values[idx] += flux;
+
+                }
+            }
+            else
+            {
+               if(flux >= 0.0){
+
+                   // it goes to the off diagonal
+                   // which neighbor this is
+                   auto& neighbor = face.get_neighbor();
+                   auto idx = element.which_neighbor_am_i(neighbor);
+                   values[idx] -= flux;
+
+               }
+             else{
+                   // the contribution goes to the
+                   // diagonal
+                   values[0] -= flux;
+               }
+            }
+        }
     }
 }
 
