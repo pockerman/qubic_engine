021a232bcc91d9d2e84270925354db2dfca5e1fb
Alexandros Giavaras <a.giavaras@gmail.com>
2020-03-12 20:25:07 +0000

    #226 Update example

--- cubic_engine/examples/exe6/exe.cpp
+++ cubic_engine/examples/exe6/exe.cpp
@@ -16,21 +16,22 @@ namespace exe
 using cengine::uint_t;
 using cengine::real_t;
 using cengine::DynMat;
+using cengine::rl::worlds::CliffWorld;
 
-// enumeration describing the action space
-enum class Action{actionNorth=0,
-                  actionSouth=1,
-                  actionEast=2,
-                  actionWest=3};
 
-// the size of the action space
-constexpr int ACTION_SPACE_SIZE = 4;
 
 }
 
 int main(int argc, char** argv) {
 
    using namespace exe;
+
+    /// the world of the agent
+    CliffWorld world(30, 15);
+
+    /// simulation parameters
+    const real_t EPSILON = 0.7;
+    const uint_t N_ITERATIONS = 10000;
 
 
 
