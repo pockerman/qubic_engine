af8f91e796eb2821d4b5c5aef08b2efb3f24a4aa
Alexandros Giavaras <a.giavaras@gmail.com>
2020-03-14 13:32:04 +0000

    #225 NOOP

--- cubic_engine/source/cubic_engine/control/pure_pursuit_path_tracker.cpp
+++ cubic_engine/source/cubic_engine/control/pure_pursuit_path_tracker.cpp
@@ -1,4 +1,5 @@
 #include "cubic_engine/control/pure_pursuit_path_tracker.h"
+#include "kernel/discretization/utils/line_mesh_utils.h"
 
 namespace cengine{
 namespace control{
@@ -23,19 +24,22 @@ PurePursuit2DPathTracker::read(path_t& r)const{
 void
 PurePursuit2DPathTracker::execute(const kernel::dynamics::SysState<2>& state){
 
-    // 1. Find the path point closest to the position
+    /// 1. Find the path point closest to the position
+
+    /// get the position coordinates
     real_t x = state.get("X");
     real_t y = state.get("Y");
 
+    /// form the position
     kernel::GeomPoint<2> position({x,y});
 
     path_t path;
     this->read(path);
 
-    //
-    const kernel::GeomPoint<2> = path.find_closest_point_to(position);
+    ///
+    const kernel::GeomPoint<2> point = kernel::numerics::find_closest_point_to(path, position);
 
-    //2. Find the lookahead point
+    /// 2. Find the lookahead point
 
     //3. calculate the curvature and return the steering
     //   request based on that curvature
