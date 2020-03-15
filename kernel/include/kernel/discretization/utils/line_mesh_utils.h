9345095ea025628396940260c5022dcde5489a04
Alexandros Giavaras <a.giavaras@gmail.com>
2020-03-15 14:59:42 +0000

    #225 Add find_intersections function

--- kernel/include/kernel/discretization/utils/line_mesh_utils.h
+++ kernel/include/kernel/discretization/utils/line_mesh_utils.h
@@ -3,27 +3,33 @@
 
 #include "kernel/geometry/geom_point.h"
 
+#include <vector>
+
 namespace kernel {
+
+/// forward declarations
+class Circle;
+
 namespace numerics {
 
 template<int dim> class LineMesh;
 
-/// \brief Find the closest point on the given LineMesh
-/// The function loops over the elements in the
-/// mesh and uses the calculation described in
-/// https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
-/// It then out of the calculated points it picks the one with the smallest
-/// distance
-const GeomPoint<2> find_closest_point_to(const LineMesh<2>& mesh, const GeomPoint<2>&p);
 
 /// \brief Find the closest point on the given LineMesh
 /// The function loops over the elements in the
-/// mesh and uses the calculation described in
-/// https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
-/// It then out of the calculated points it picks the one with the smallest
-/// distance
+/// mesh and uses samples the EdgeElement using the given
+/// number of sampling points plus two as it accounts for the
+/// vertices as well. It then out of the calculated points
+/// it picks the one with the smallest distance
 const GeomPoint<2> find_closest_point_to(const LineMesh<2>& mesh,
                                          const GeomPoint<2>&p, uint_t nsamples);
+
+/// \brief Returns the intersection points of the
+/// Circle with the elements of the LineMesh.
+/// The algorithn implemented is the one described here
+/// https://stackoverflow.com/questions/1073336/circle-line-segment-collision-detection-algorithm/1084899#1084899%E2%80%8B
+const std::vector<GeomPoint<2>> find_intersections(const LineMesh<2>& mesh,
+                                                   const Circle& circle);
 
 }
 
