2b6bbd225a2f2277dc096d97ddb5fdc5acadc6f7
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-02 20:34:02 +0100

    #226 Update example

--- cubic_engine/examples/exe6/exe.cpp
+++ cubic_engine/examples/exe6/exe.cpp
@@ -1,6 +1,7 @@
 #include "cubic_engine/base/cubic_engine_types.h"
 #include "kernel/utilities/csv_file_writer.h"
-#include "cubic_engine/rl/worlds/cliff_world.h"
+#include "cubic_engine/rl/worlds/grid_world.h"
+#include "cubic_engine/rl/worlds/grid_world_action_space.h"
 
 
 #include <cmath>
@@ -16,7 +17,13 @@ namespace exe
 using cengine::uint_t;
 using cengine::real_t;
 using cengine::DynMat;
-using cengine::rl::worlds::CliffWorld;
+using cengine::rl::worlds::GridWorld;
+using cengine::rl::worlds::GridWorldAction;
+
+class RewardProducer
+{
+
+};
 
 
 
@@ -24,10 +31,15 @@ using cengine::rl::worlds::CliffWorld;
 
 int main(int argc, char** argv) {
 
-   using namespace exe;
+    using namespace exe;
+
+    typedef GridWorld<GridWorldAction, RewardProducer>::state_t state_t;
 
     /// the world of the agent
-    CliffWorld world(30, 15);
+    GridWorld<GridWorldAction, RewardProducer> world(7, 7);
+
+    state_t start = 10;
+    state_t goal = 12;
 
     /// simulation parameters
     const real_t EPSILON = 0.7;
@@ -37,7 +49,7 @@ int main(int argc, char** argv) {
     const uint_t N_ITERATIONS = 10000;
 
     for(uint_t episode=0; episode < N_ITERATIONS; ++episode){
-        world.restart();
+        world.restart(start, goal);
     }
 
 
