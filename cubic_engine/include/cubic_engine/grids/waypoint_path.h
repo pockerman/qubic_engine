99ca37d827e8fad53a9586d6106f697c46901b79
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-10 15:03:56 +0100

    #225 Update API

--- cubic_engine/include/cubic_engine/grids/waypoint_path.h
+++ cubic_engine/include/cubic_engine/grids/waypoint_path.h
@@ -5,6 +5,8 @@
 #include "kernel/geometry/generic_line.h"
 
 #include <vector>
+#include <stdexcept>
+
 namespace cengine{
 namespace grids {
 
@@ -12,10 +14,26 @@ namespace grids {
 template<int dim, typename Data>
 struct WayPoint
 {
-    kernel::GeomPoint<dim> position;
+    typedef kernel::GeomPoint<dim> position_t;
+    typedef Data data_t;
+    position_t position;
     uint_t id;
     Data data;
 
+    /// constructor
+    WayPoint(const position_t& p, uint_t id_, const data_t& data_=data_t())
+        :
+          position(p),
+          id(id_),
+          data(data)
+    {}
+};
+
+struct LineSegmentData
+{
+    /// \brief The maximum velocity
+    /// allowed on the edge
+    real_t Vmax;
 };
 
 template<int dim, typename NodeData, typename SegmentData>
@@ -23,6 +41,8 @@ class LineSegment: private kernel::kernel_detail::generic_line_base<WayPoint<dim
 {
 public:
 
+    typedef NodeData node_data_t;
+    typedef SegmentData segment_data_t;
     typedef WayPoint<dim, NodeData> w_point_t;
     typedef kernel::kernel_detail::generic_line_base<w_point_t> base;
     typedef typename base::node_type node_type;
@@ -33,14 +53,31 @@ public:
     using base::set_id;
     using base::has_valid_id;
 
-private:
+    /// \brief Constructor
+    LineSegment(uint_t id, const w_point_t& v1,
+                const w_point_t& v2, const segment_data_t& data);
 
+private:
 
     /// \brief list of internal points of
     /// the segment.
     std::vector<w_point_t> internal_points_;
 
+    /// \brief The data asociated with the segmen
+    segment_data_t data_;
+
 };
+
+template<int dim, typename NodeData, typename SegmentData>
+LineSegment<dim, NodeData, SegmentData>::LineSegment(uint_t id,
+                                                     const typename LineSegment<dim, NodeData, SegmentData>::w_point_t& v1,
+                                                     const typename LineSegment<dim, NodeData, SegmentData>::w_point_t& v2,
+                                                     const typename LineSegment<dim, NodeData, SegmentData>::segment_data_t& data)
+    :
+     kernel::kernel_detail::generic_line_base<WayPoint<dim, NodeData>>(v1, v2, id),
+     internal_points_(),
+     data_(data)
+{}
 
 /// \brief class WaypointPath models a path formed
 /// by line segments and way points. The Data
@@ -67,12 +104,17 @@ public:
     /// \brief Constructor
     WaypointPath();
 
+    /// \brief How many waypoints the pah has
+    uint_t n_wpoints()const{return waypoints_.size();}
+
+    /// \brief How many segments the path has
+    uint_t n_segments()const{return segments_.size();}
+
     /// \brief Reserve space for waypoints
     void reserve_n_waypoints(uint_t n);
 
     /// \brief Reserve space for
     void reserve_n_segments(uint_t n);
-
 
     /// \brief Add a new waypoint in the path and get back
     /// a writable reference of the newly added point
@@ -80,7 +122,9 @@ public:
                              const w_point_data_t& data);
 
     /// \brief Add a new segment in the path and get back
-    /// a writable reference of the newly added segment
+    /// a writable reference of the newly added segment.
+    /// Throws std::logic_error if vid0 and vid1 are not
+    /// in the path
     segment_t& add_segment(uint_t vid0, uint_t vid1,
                             const segment_data_t& data);
 
@@ -109,6 +153,61 @@ private:
     std::vector<segment_t> segments_;
 
 };
+
+template<int dim, typename PointData, typename EdgeData>
+WaypointPath<dim, PointData, EdgeData>::WaypointPath()
+    :
+      waypoints_(),
+      segments_()
+{}
+
+template<int dim, typename PointData, typename EdgeData>
+typename WaypointPath<dim, PointData, EdgeData>::w_point_t&
+WaypointPath<dim, PointData, EdgeData>::add_way_point(const kernel::GeomPoint<dim>& position,
+                                                      const typename WaypointPath<dim, PointData, EdgeData>::w_point_data_t& data){
+
+    uint_t id = waypoints_.size();
+    WaypointPath<dim, PointData, EdgeData>::w_point_t p(position, id, data);
+    waypoints_.push_back(p);
+    return waypoints_[id];
+}
+
+template<int dim, typename PointData, typename EdgeData>
+typename WaypointPath<dim, PointData, EdgeData>::segment_t&
+WaypointPath<dim, PointData, EdgeData>::add_segment(uint_t vid0, uint_t vid1,
+                            const typename WaypointPath<dim, PointData, EdgeData>::segment_data_t& data){
+
+    if(vid0 >= waypoints_.size() ||
+            vid1 >= waypoints_.size()){
+
+        throw std::logic_error("Waypoint id given not in range [0, " +
+                               std::to_string(waypoints_.size()) + ")");
+
+    }
+
+    if(vid0 == vid1){
+       throw std::logic_error("Cannot create segment having identical vertices");
+    }
+
+    auto id = segments_.size();
+    auto v0 = waypoints_[vid0];
+    auto v1 = waypoints_[vid1];
+    WaypointPath<dim, PointData, EdgeData>::segment_t seg(id, v0, v1, data);
+    segments_.push_back(seg);
+    return segments_[id];
+}
+
+template<int dim, typename PointData, typename EdgeData>
+void
+WaypointPath<dim, PointData, EdgeData>::reserve_n_waypoints(uint_t n){
+    waypoints_.reserve(n);
+}
+
+template<int dim, typename PointData, typename EdgeData>
+void
+WaypointPath<dim, PointData, EdgeData>::reserve_n_segments(uint_t n){
+    segments_.reserve(n);
+}
 
 }
 }
