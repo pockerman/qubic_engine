2540e5d8ddd73338fc2f16f3a13c6b427c089999
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-10 15:01:42 +0100

    #225 Add test for WaypointPath class

---
+++ cubic_engine/tests/test_waypoint_path/test.cpp
@@ -0,0 +1,55 @@
+#include "cubic_engine/grids/waypoint_path.h"
+#include "kernel/geometry/geom_point.h"
+#include "kernel/base/types.h"
+
+#include <vector>
+#include <gtest/gtest.h>
+
+TEST(TestWaypointPath, TestAddWaiPoint) {
+
+    using namespace cengine;
+    using kernel::Null;
+    using kernel::GeomPoint;
+
+    grids::WaypointPath<2,Null, Null> path;
+    path.add_way_point(GeomPoint<2>(0.0), Null());
+    ASSERT_EQ(path.n_wpoints(), 1);
+}
+
+TEST(TestWaypointPath, TestAddSegmentFail) {
+
+    using namespace cengine;
+    using kernel::Null;
+    using kernel::GeomPoint;
+
+    grids::WaypointPath<2,Null, Null> path;
+    ASSERT_THROW(path.add_segment(0, 0, Null()), std::logic_error);
+}
+
+TEST(TestWaypointPath, TestAddSegmentFail2) {
+
+    using namespace cengine;
+    using kernel::Null;
+    using kernel::GeomPoint;
+
+    grids::WaypointPath<2,Null, Null> path;
+    path.add_way_point(GeomPoint<2>(0.0), Null());
+    path.add_way_point(GeomPoint<2>(1.0), Null());
+    ASSERT_THROW(path.add_segment(0, 0, Null()), std::logic_error);
+}
+
+TEST(TestWaypointPath, TestAddSegment) {
+
+    using namespace cengine;
+    using kernel::Null;
+    using kernel::GeomPoint;
+
+    grids::WaypointPath<2,Null, Null> path;
+    path.add_way_point(GeomPoint<2>(0.0), Null());
+    path.add_way_point(GeomPoint<2>(1.0), Null());
+
+    ASSERT_EQ(path.n_wpoints(), 2);
+
+    path.add_segment(0, 1, Null());
+    ASSERT_EQ(path.n_segments(), 1);
+}
