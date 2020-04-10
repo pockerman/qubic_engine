0dd8a59577586e9fb525fb151153eb49711584bf
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-10 15:03:40 +0100

    #225 Update API

--- cubic_engine/include/cubic_engine/control/bang_bang_path_track_controler.h
+++ cubic_engine/include/cubic_engine/control/bang_bang_path_track_controler.h
@@ -2,6 +2,12 @@
 #define BANG_BANG_PATH_TRACK_CONTROLER_H
 
 #include "cubic_engine/base/cubic_engine_types.h"
+#include "cubic_engine/grids/waypoint_path.h"
+#include "kernel/geometry/geom_point.h"
+#include "kernel/dynamics/system_state.h"
+#include "kernel/patterns/observer_base.h"
+
+#include "boost/noncopyable.hpp"
 namespace cengine {
 namespace control {
 
@@ -12,14 +18,16 @@ namespace control {
 /// Journal of Intelligent and Robotic Systems
 /// proposed by K. C. KOH and H. S. CHO
 ///
-///
-class BangBangPathTrackController
+
+template<typename PointData, typename SegmentData>
+class BangBangPathTrackController:public kernel::ObserverBase<grids::WaypointPath<2, PointData, SegmentData>*>,
+                                  private boost::noncopyable
 {
 public:
 
 
     /// \brief Execute controller
-    void execute()const;
+    void execute(const kernel::dynamics::SysState<3>& state)const;
 
 
 private:
@@ -29,6 +37,18 @@ private:
 
     /// \brief Maximum acceleration
     real_t amax_;
+
+    /// \brief The lookahead distance
+    real_t lookahead_dist_;
+
+    /// \brief number of sampling poinst to use
+    /// when computing the closest point from
+    /// position to the path
+    uint_t n_sampling_points_;
+
+    /// \brief The lookahed point calculated
+    /// by the controller
+    kernel::GeomPoint<2> lookahead_point_;
 };
 
 }
