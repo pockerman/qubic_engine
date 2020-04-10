2cb3964544e6920e93aafc6d00accea854448605
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-10 15:04:17 +0100

    #225 Update example implementation

--- cubic_engine/examples/exe21/exe.cpp
+++ cubic_engine/examples/exe21/exe.cpp
@@ -1,5 +1,7 @@
 #include "cubic_engine/base/cubic_engine_types.h"
 #include "cubic_engine/control/pure_pursuit_path_tracker.h"
+#include "cubic_engine/control/bang_bang_path_track_controler.h"
+#include "cubic_engine/grids/waypoint_path.h"
 #include "kernel/discretization/line_mesh.h"
 #include "kernel/geometry/geom_point.h"
 #include "kernel/discretization/edge_element.h"
@@ -16,9 +18,15 @@ namespace example
 using cengine::uint_t;
 using cengine::real_t;
 using cengine::control::PurePursuit2DPathTracker;
-using kernel::numerics::LineMesh;
+using cengine::control::BangBangPathTrackController;
+using cengine::grids::WaypointPath;
+using cengine::grids::WayPoint;
+using cengine::grids::LineSegment;
+using cengine::grids::LineSegmentData;
 using kernel::GeomPoint;
 using kernel::dynamics::SysState;
+using kernel::DiffDriveProperties;
+using kernel::Null;
 
 const real_t DT = 0.5;
 
@@ -28,18 +36,20 @@ class Agent
 public:
 
     /// Constructor
-    Agent(const LineMesh<2>& path, const GeomPoint<2>& goal, real_t goal_r);
+    Agent(const WaypointPath<2, Null, LineSegmentData>& path,
+          const GeomPoint<2>& goal,
+          real_t goal_r, const  DiffDriveProperties& properties);
 
-    /// execute the agent i.e. go to goal
+    /// execute the agent that is go to goal
     void execute();
 
     /// returns trus  if the goal has been reached
-    bool goal_reched()const;
+    bool goal_reached()const;
 
 private:
 
     /// the path the agent must follow
-    const LineMesh<2>* path_ptr_;
+    const WaypointPath<2, Null, LineSegmentData>* path_ptr_;
 
     /// mobile platform of the agent
     kernel::DiffDriveVehicle platform_;
@@ -54,17 +64,19 @@ private:
     real_t g_radius_;
 };
 
-Agent::Agent(const LineMesh<2>& path, const GeomPoint<2>& goal, real_t goal_r)
+Agent::Agent(const WaypointPath<2, Null, LineSegmentData>& path,
+             const GeomPoint<2>& goal,
+             real_t goal_r, const  DiffDriveProperties& properties)
     :
     path_ptr_(&path),
-    platform_(0.2),
+    platform_(properties),
     tracker_(),
     goal_(goal),
     g_radius_(goal_r)
 {}
 
 bool
-Agent::goal_reched()const{
+Agent::goal_reached()const{
 
     GeomPoint<2> pos;
     platform_.get_position(pos);
@@ -80,22 +92,36 @@ void Agent::execute(){
 
 
     /// update the path the tracker is using
+    /// and the various properties
     tracker_.update(*path_ptr_);
     tracker_.set_n_sampling_points(4);
     tracker_.set_lookahead_dist(0.2);
 
     real_t time = 0.0;
 
-    while(!goal_reched()){
+    while(!goal_reached()){
 
         /// let's see the state of the agent
         std::cout<<"At time: "<<time<<std::endl;
         std::cout<<platform_.get_state()<<std::endl;
 
-        platform_.integrate(0.0, 0.0);
-
         /// correct for the path
         tracker_.execute(platform_.get_state());
+
+        /// derive the ref vr and vl required to
+        /// follow the path
+
+        /// apply control to the derived vr_ref and
+        /// vl_ref
+
+
+        /// ...and feed the result back to the system
+
+        platform_.integrate(0.0, 0.0);
+
+
+
+
 
         time += DT;
     }
@@ -108,27 +134,24 @@ int main(){
 
     using namespace example;
 
+    const real_t GOAL_RADIUS = 0.2;
     try{
 
         /// the path to follow
-        LineMesh<2> path;
+        WaypointPath<2, Null, LineSegmentData> path;
+        path.reserve_n_waypoints(3);
 
-        /// create the path
-        auto node0 = path.add_node(GeomPoint<2>({0.0, 0.0}));
-        auto node1 = path.add_node(GeomPoint<2>({1.0, 1.0}));
-        auto node2 = path.add_node(GeomPoint<2>({2.0, 0.0}));
+        path.add_way_point(GeomPoint<2>({0.0, 0.0}), Null());
+        path.add_way_point(GeomPoint<2>({1.0, 1.0}), Null());
+        path.add_way_point(GeomPoint<2>({2.0, 0.0}), Null());
 
-        auto element = path.add_element();
-        element->resize_nodes();
-        element->set_node(0, node0);
-        element->set_node(1, node1);
+        path.reserve_n_segments(2);
+        path.add_segment(0, 1, LineSegmentData());
+        path.add_segment(1, 2, LineSegmentData());
 
-        element = path.add_element();
-        element->resize_nodes();
-        element->set_node(0, node1);
-        element->set_node(1, node2);
+        DiffDriveProperties properties;
 
-        Agent agent(path, *node2, 0.2);
+        Agent agent(path, *node2, GOAL_RADIUS, properties);
         agent.execute();
     }
     catch(std::exception& e){
