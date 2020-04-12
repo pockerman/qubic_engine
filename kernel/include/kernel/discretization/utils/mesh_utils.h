a1d077c948a9e790b5525a7115296b8176062dd0
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-12 21:27:22 +0100

    #225 Refactoring

--- kernel/include/kernel/discretization/utils/mesh_utils.h
+++ kernel/include/kernel/discretization/utils/mesh_utils.h
@@ -14,11 +14,76 @@ namespace kernel {
 namespace discretization {
 namespace utils {
 
+
+
+template<typename ElementTp>
+real_t distance_from(const ElementTp& element,
+                     const GeomPoint<ElementTp::dimension>& p,
+                     uint_t nsamples, real_t tol){
+
+    typedef GeomPoint<ElementTp::dimension> point_t;
+
+    std::vector<point_t> points_on_segment(nsamples + 2);
+
+    auto v0 = element.get_vertex(0);
+    auto v1 = element.get_vertex(1);
+
+    real_t distance = v0.distance(v1);
+
+    ///...plus 2 because we also account for the end poinst
+    real_t h = distance/(real_t) (nsamples + 2);
+
+    /// the first point is the staring vertex
+    points_on_segment[0] = v0;
+
+    if(std::fabs(v1[0] - v0[0]) < tol){
+        /// we have line that is perpendicular to the
+        /// x-axis and we need to act differently to avoid infinity
+
+        for(uint_t sp = 1; sp <=nsamples; ++sp){
+
+            real_t xi = v0[0];
+            points_on_segment[sp] = point_t({xi, v0[0] + sp*h});
+        }
+    }
+    else if(std::fabs(v1[1] - v0[1]) < tol){
+        /// we have line that is parallel to the
+        /// x-axis and we need to act differently to avoid infinity
+
+        for(uint_t sp = 1; sp <=nsamples; ++sp){
+
+            real_t xi = v0[1];
+            points_on_segment[sp] = point_t({v0[0] + sp*h, xi});
+        }
+
+    }
+    else{
+
+        /// calculate the coefficients of the line
+        /// connecting the two vertices
+        real_t alpha = (v1[1] - v0[1])/(v1[0] - v0[0]);
+        real_t beta = (v0[1]*v1[0] + v0[0]*v1[1])/(v1[0] - v0[0]);
+
+        for(uint_t sp = 1; sp <=nsamples; ++sp){
+
+            real_t xi = v0[0] + sp*h;
+            points_on_segment[sp] = point_t({xi, alpha*xi + beta});
+        }
+    }
+    /// add the ending vertex of the line segment
+    points_on_segment[nsamples + 1] = v1;
+
+    auto min_point = get_point_with_min_distance(p, points_on_segment);
+    return  min_point.distance(p);
+}
+
+
 /// \brief Find the element in the mesh
 /// with the smallest distance from the point
 template<typename MeshTp>
 const typename MeshTp::element_t*
-find_closest_element(const MeshTp& mesh, const GeomPoint<MeshTp::dimension>& p){
+find_closest_element(const MeshTp& mesh, const GeomPoint<MeshTp::dimension>& p,
+                     uint_t nsamples, real_t tol){
 
     numerics::ConstElementMeshIterator<numerics::Active, MeshTp> filter(mesh);
     typedef typename MeshTp::element_t element_t;
@@ -31,7 +96,7 @@ find_closest_element(const MeshTp& mesh, const GeomPoint<MeshTp::dimension>& p){
     for(; begin != end; ++begin){
 
         auto element = *begin;
-        auto dist_cur = element->distance_from(p);
+        auto dist_cur = distance_from(*element, p, nsamples, tol);
 
         if(dist_cur < dist){
             dist = dist_cur;
@@ -45,7 +110,7 @@ find_closest_element(const MeshTp& mesh, const GeomPoint<MeshTp::dimension>& p){
 
 template<typename MeshTp>
 const typename MeshTp::element_t*
-find_first_closest_element(const MeshTp& mesh, const GeomPoint<MeshTp::dimension>& p, real_t tol){
+find_first_closest_element(const MeshTp& mesh, const GeomPoint<MeshTp::dimension>& p, uint_t nsamples, real_t tol){
 
 
     numerics::ConstElementMeshIterator<numerics::Active, MeshTp> filter(mesh);
@@ -58,7 +123,7 @@ find_first_closest_element(const MeshTp& mesh, const GeomPoint<MeshTp::dimension
     for(; begin != end; ++begin){
 
         auto element = *begin;
-        auto dist_cur = element->distance_from(p);
+        auto dist_cur = distance_from(*element, p, nsamples, tol);
 
         if(dist_cur < tol){
 
@@ -113,6 +178,17 @@ find_closest_point_to(const MeshTp& mesh, const GeomPoint<2>& p,
                 real_t xi = v0[0];
                 points_on_segment[sp] = point_t({xi, v0[0] + sp*h});
             }
+        }
+        else if(std::fabs(v1[1] - v0[1]) < tol){
+            /// we have line that is parallel to the
+            /// x-axis and we need to act differently to avoid infinity
+
+            for(uint_t sp = 1; sp <=nsamples; ++sp){
+
+                real_t xi = v0[1];
+                points_on_segment[sp] = point_t({v0[0] + sp*h, });
+            }
+
         }
         else{
 
