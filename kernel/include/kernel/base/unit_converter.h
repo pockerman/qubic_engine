bf912500047c497006df144384886d03955d7a6e
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-13 16:00:45 +0100

    #225 API update

--- kernel/include/kernel/base/unit_converter.h
+++ kernel/include/kernel/base/unit_converter.h
@@ -22,6 +22,12 @@ public:
     /// \brief Convert RPM to angular speed in m/s
     static real_t angular_speed_to_linear_speed(real_t r, real_t w){return r*w;}
 
+    /// \brief Convert radians to degrees
+    static real_t rad_to_degrees(real_t rad){return (rad*180.0)/MathConsts::PI;}
+
+    /// \brief Convert degrees to radians
+    static real_t degrees_to_rad(real_t degs){return (degs*MathConsts::PI)/180.0;}
+
 };
 }
 
