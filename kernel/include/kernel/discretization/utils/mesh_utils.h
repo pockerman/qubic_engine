a028cb6aae084e019485ab2b20f473e918eb5358
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-13 16:00:22 +0100

    #225 Refactoring

--- kernel/include/kernel/discretization/utils/mesh_utils.h
+++ kernel/include/kernel/discretization/utils/mesh_utils.h
@@ -14,6 +14,69 @@ namespace kernel {
 namespace discretization {
 namespace utils {
 
+template<typename ElementTp>
+const kernel::GeomPoint<ElementTp::dimension>
+find_point_on_element_closest_to(const ElementTp& element, const GeomPoint<ElementTp::dimension>& p,
+                                 uint_t nsamples, real_t tol){
+
+    static_assert (ElementTp::dimension == 2, "Element dimension is not 2");
+    typedef kernel::GeomPoint<ElementTp::dimension> point_t;
+
+    std::vector<point_t> points_on_segment(nsamples + 2);
+
+    auto v0 = element.get_vertex(0);
+    auto v1 = element.get_vertex(1);
+
+    real_t distance = v0.distance(v1);
+
+    ///...plus 2 because we also account for the end poinst
+    real_t h = distance/(real_t) (nsamples + 2);
+
+    /// the first point is the staring vertex
+    points_on_segment[0] = v0;
+
+    /// the first point is the staring vertex
+    points_on_segment[0] = v0;
+
+    if(std::fabs(v1[0] - v0[0]) < tol){
+        /// we have line that is perpendicular to the
+        /// x-axis and we need to act differently to avoid infinity
+
+        for(uint_t sp = 1; sp <=nsamples; ++sp){
+
+            real_t xi = v0[0];
+            points_on_segment[sp] = point_t({xi, v0[0] + sp*h});
+        }
+    }
+    else if(std::fabs(v1[1] - v0[1]) < tol){
+        /// we have line that is parallel to the
+        /// x-axis and we need to act differently to avoid infinity
+
+        for(uint_t sp = 1; sp <=nsamples; ++sp){
+
+            real_t xi = v0[1];
+            points_on_segment[sp] = point_t({v0[0] + sp*h, xi});
+        }
+    }
+    else{
+
+        /// calculate the coefficients of the line
+        /// connecting the two vertices
+        real_t alpha = (v1[1] - v0[1])/(v1[0] - v0[0]);
+        real_t beta = (v0[1]*v1[0] + v0[0]*v1[1])/(v1[0] - v0[0]);
+
+        for(uint_t sp = 1; sp <=nsamples; ++sp){
+
+            real_t xi = v0[0] + sp*h;
+            points_on_segment[sp] = point_t({xi, alpha*xi + beta});
+        }
+    }
+    /// add the ending vertex of the line segment
+    points_on_segment[nsamples + 1] = v1;
+
+    auto min_point = get_point_with_min_distance(p, points_on_segment);
+    return min_point;
+}
 
 
 template<typename ElementTp>
@@ -21,7 +84,9 @@ real_t distance_from(const ElementTp& element,
                      const GeomPoint<ElementTp::dimension>& p,
                      uint_t nsamples, real_t tol){
 
-    typedef GeomPoint<ElementTp::dimension> point_t;
+
+    auto point = find_point_on_element_closest_to(element, p, nsamples, tol);
+    /*typedef GeomPoint<ElementTp::dimension> point_t;
 
     std::vector<point_t> points_on_segment(nsamples + 2);
 
@@ -73,8 +138,8 @@ real_t distance_from(const ElementTp& element,
     /// add the ending vertex of the line segment
     points_on_segment[nsamples + 1] = v1;
 
-    auto min_point = get_point_with_min_distance(p, points_on_segment);
-    return  min_point.distance(p);
+    auto min_point = get_point_with_min_distance(p, points_on_segment);*/
+    return  point.distance(p);
 }
 
 
@@ -108,33 +173,7 @@ find_closest_element(const MeshTp& mesh, const GeomPoint<MeshTp::dimension>& p,
 }
 
 
-template<typename MeshTp>
-const typename MeshTp::element_t*
-find_first_closest_element(const MeshTp& mesh, const GeomPoint<MeshTp::dimension>& p, uint_t nsamples, real_t tol){
 
-
-    numerics::ConstElementMeshIterator<numerics::Active, MeshTp> filter(mesh);
-    typedef typename MeshTp::element_t element_t;
-    element_t* ptr = nullptr;
-
-    auto begin = filter.begin();
-    auto end   = filter.end();
-
-    for(; begin != end; ++begin){
-
-        auto element = *begin;
-        auto dist_cur = distance_from(*element, p, nsamples, tol);
-
-        if(dist_cur < tol){
-
-            ptr = element;
-            break;
-        }
-    }
-
-    return ptr;
-
-}
 
 template<typename MeshTp>
 std::tuple<const typename MeshTp::point_t, const typename MeshTp::element_t*>
@@ -145,7 +184,7 @@ find_closest_point_to(const MeshTp& mesh, const GeomPoint<2>& p,
 
     typedef typename MeshTp::point_t point_t;
 
-    std::vector<point_t> points_on_segment(nsamples + 2);
+    //std::vector<point_t> points_on_segment(nsamples + 2);
     std::vector<point_t> min_points;
     min_points.reserve(mesh.n_elements());
 
@@ -158,7 +197,7 @@ find_closest_point_to(const MeshTp& mesh, const GeomPoint<2>& p,
 
         auto element = *begin;
 
-        auto v0 = element->get_vertex(0);
+        /*auto v0 = element->get_vertex(0);
         auto v1 = element->get_vertex(1);
 
         real_t distance = v0.distance(v1);
@@ -186,9 +225,8 @@ find_closest_point_to(const MeshTp& mesh, const GeomPoint<2>& p,
             for(uint_t sp = 1; sp <=nsamples; ++sp){
 
                 real_t xi = v0[1];
-                points_on_segment[sp] = point_t({v0[0] + sp*h, });
+                points_on_segment[sp] = point_t({v0[0] + sp*h, xi});
             }
-
         }
         else{
 
@@ -207,7 +245,8 @@ find_closest_point_to(const MeshTp& mesh, const GeomPoint<2>& p,
         points_on_segment[nsamples + 1] = v1;
 
         /// loop over the points and find the point with the minimum distance
-        auto min_point = get_point_with_min_distance(p, points_on_segment);
+        auto min_point = get_point_with_min_distance(p, points_on_segment);*/
+        auto min_point = find_point_on_element_closest_to(*element, p, nsamples, tol);
         min_points.push_back(min_point);
 
     }
@@ -215,7 +254,7 @@ find_closest_point_to(const MeshTp& mesh, const GeomPoint<2>& p,
     auto result = get_point_with_min_distance(p, min_points);
 
     /// find out to which segment this point belongs to
-    auto element = find_first_closest_element(mesh, result, tol);
+    auto element = find_closest_element(mesh, result, nsamples, tol);
 
     return {result, element};
 }
