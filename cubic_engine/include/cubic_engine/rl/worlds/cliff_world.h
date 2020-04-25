e5c6d17db093774a06e0524ad7b0e4a0fd67329e
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-25 21:17:31 +0100

    #251 Fix CliffWorld and other bugs

--- cubic_engine/include/cubic_engine/rl/worlds/cliff_world.h
+++ cubic_engine/include/cubic_engine/rl/worlds/cliff_world.h
@@ -4,42 +4,14 @@
 #include "cubic_engine/base/cubic_engine_types.h"
 #include "cubic_engine/rl/world.h"
 #include "cubic_engine/rl/worlds/grid_world_action_space.h"
+#include "cubic_engine/rl/worlds/grid_world_state.h"
+
+#include  <stdexcept>
 
 namespace cengine{
 namespace rl{
 namespace worlds {
 
-// The phase space
-
-// This defines external world. Here, we have the following descrete
-// world. '#' is a cliff. When the agent goes there, is restarts at
-// state S and is given a -CLIFF_REWARD reward. Otherwise, each
-// transition gives a -STEP_REWARD reward, except from the goal G that
-// leads to a terminal state for all actions.
-
-/*
- *   <----LENGTH---->
- *   ................ line number WIDTH-1
- *   ................
- *   ................
- *   ................ line number 0
- *   S##############G
- */
-
-// The phase space is thus an integer, that we will define by an
-// enum. Ths class Cliff handles the mapping to actual grid
-// positions shown on the previous figure into an integer.
-
-
-/**
- * Cliff-Walking  parameter handler.
- * @author <a href="mailto:Herve.Frezza-Buet@supelec.fr">Herve.Frezza-Buet@supelec.fr</a>
- */
-
-
-/// \brief Enumeration describing the action
-/// space for the Cliff-World problem
-using CliffWorldAction=GridWorldAction;
 
 struct CliffWorldReward
 {
@@ -59,45 +31,39 @@ struct CliffWorldReward
 
 /// \brief CliffWorld class models the Environment
 /// for the cliff-world problem
-class CliffWorld: public World<CliffWorldAction, int, real_t>
+class CliffWorld: public World<GridWorldAction, GridWorldState, real_t>
 {
 
 public:
 
-    typedef World<CliffWorldAction, int, real_t>::state_t state_t;
-    typedef World<CliffWorldAction, int, real_t>::reward_value_t reward_value_t;
+    typedef real_t reward_t;
+    typedef typename World<GridWorldAction, GridWorldState, real_t>::action_t action_t;
+    typedef typename World<GridWorldAction, GridWorldState, real_t>::state_t state_t;
+    typedef typename World<GridWorldAction, GridWorldState, real_t>::reward_value_t reward_value_t;
 
-    /// \brief the starting state of the world
-    static const state_t  START = 0;
+    /// \brief Global invalid action assumed by the
+    /// world. Useful for initialization.
+    static const GridWorldAction invalid_action = GridWorldAction::INVALID_ACTION;
 
     /// \brief Constructor
-    CliffWorld(uint_t xlength,  uint_t ylength);
+    CliffWorld();
 
     /// \brief Destructor
     ~CliffWorld();
 
     /// \brief Returns the state
-    virtual state_t& sense()override final{return  agent_pos_;}
+    virtual const state_t& sense()override final{return  *current_state_;}
 
     /// \brief Transition to a new state by
     /// performing the given action
     virtual void step(const action_t&)override final;
 
-    /// \brief Execute the start step
-    void step_start(const CliffWorld::action_t& action);
-
-    /// \brief Execute the stop step
-    void step_goal(const CliffWorld::action_t& action);
-
-    /// \brief Execute any other step
-    void do_step(const CliffWorld::action_t& action);
-
     /// \brief Restart the world. This means that
-    /// the stare of the world will be that of...
-    void restart();
+    /// the state of the world will be that of...
+    void restart(const state_t& start, const state_t& goal);
 
     /// \brief Set the state of the world
-    void set_state(const state_t state);
+    void set_state(const state_t& state){current_state_ = &state;}
 
     /// \brief Returns the reward associated
     /// with the last state transition
@@ -106,35 +72,114 @@ public:
     /// \brief The size of the world.  Namely,
     /// the total number of different positions in
     /// the grid
-    uint_t size()const{return xlength_ * ylength_ + 2;}
+    uint_t size()const{return states_.size();}
+
+    /// \brief Set up the cells map
+    void set_states(std::vector<state_t>&& states);
+
+    /// \brief returns the current state of the world
+    const state_t& get_current_state()const{return *current_state_; }
+
+    /// \brief Execute the aid-th action in the current state
+    void execute_action(action_t aid);
+
+    /// \brief The number of states of the world
+    uint_t n_states()const{return states_.size();}
+
+    /// \brief Returns the i-th state
+    state_t& get_state(uint_t s);
+
+    /// \brief Returns the i-th state
+    const state_t& get_state(uint_t s)const;
+
+    /// \brief Returns true if the world is finished
+    bool is_finished()const{return finished_;}
+
 
 private:
 
-    /// \brief The length  of the world in the x direction
-    uint_t xlength_;
+    /// \brief The starting state
+    const state_t* start_;
 
-    /// \brief The length of the world in the y direction
-    uint_t ylength_;
+    /// \brief The goal state
+    const state_t* goal_;
 
-    /// \brief The starting position
-    uint_t start_{0};
+    /// \brief The current state the world is in
+    const state_t* current_state_;
 
-    /// \brief The goal position
-    uint_t goal_;
-
-    /// \brief The current position
-    /// of the agent in the world describes
-    /// the state of the world. Initially this is simply
-    /// the starting position
-    state_t agent_pos_{0};
-
-    /// \brief Object responsible for returning the rewards
-    CliffWorldReward reward_;
+    /// \brief The object responsible for
+    /// producing the reward that the agent
+    /// should receive
+    reward_t reward_;
 
     /// \brief The reward that the agent should recieve
     /// after executing an action
     reward_value_t r_;
+
+    /// \brief A map that describes the possible state transitions
+    /// from one state to another. This is simply a list of
+    /// cell ids the agent can transition to
+    std::vector<state_t> states_;
+
+    /// \brief Flag indicating if the world
+    /// has the current_state_ and goal_ state
+    /// equal
+    bool finished_;
 };
+
+inline
+void
+CliffWorld::execute_action(CliffWorld::action_t aid){
+    current_state_ = const_cast<state_t*>(current_state_)->execute_action(aid);
+}
+
+inline
+void
+CliffWorld::set_states(std::vector<CliffWorld::state_t>&& states){
+    states_ = states;
+}
+
+inline
+void
+CliffWorld::restart(const typename CliffWorld::state_t& start,
+                    const typename CliffWorld::state_t& goal){
+
+    start_ = &start;
+    goal_ = &goal;
+    current_state_ = &get_state(start_->get_id());
+    r_ = 0.0;
+    finished_ = false;
+
+}
+
+
+inline
+const CliffWorld::state_t&
+CliffWorld::get_state(uint_t id)const{
+
+    if(id >= states_.size()){
+        throw std::logic_error("Invalid state id: "+
+                               std::to_string(id) +
+                               " not in [0, "+
+                               std::to_string(states_.size()));
+    }
+
+    return states_[id];
+}
+
+inline
+CliffWorld::state_t&
+CliffWorld::get_state(uint_t id){
+
+    if(id >= states_.size()){
+        throw std::logic_error("Invalid state id: "+
+                               std::to_string(id) +
+                               " not in [0, "+
+                               std::to_string(states_.size()));
+    }
+
+    return states_[id];
+}
 
 }
 }
