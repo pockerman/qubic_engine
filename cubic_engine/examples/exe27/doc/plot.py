13d5cb34bc6c109db78a5607f80334ad37214e27
Alexandros Giavaras <a.giavaras@gmail.com>
2020-05-31 14:05:03 +0100

    #29 Update exaample

--- cubic_engine/examples/exe27/doc/plot.py
+++ cubic_engine/examples/exe27/doc/plot.py
@@ -13,46 +13,43 @@ def main():
   with open('state.csv', 'r') as file:
     csv_reader = csv.reader(file, delimiter=",")
 
-    x1 = []
-    y1 = []
+    xpos = []
+    ypos = []
 
-    x2 = []
-    y2 = []
 
     for line in csv_reader:
-          plt.cla()
-          plt.gcf().canvas.mpl_connect('key_release_event',
-							lambda event: [exit(0) if event.key == 'escape'
-                                                   else None])
 
-          if counter != 0:
-              xe = float(line[0])
-              ye = float(line[1])
-              xa = float(line[2])
-              ya = float(line[3])
-              x1.append(xe)
-              y1.append(ye)
-              x2.append(xa)
-              y2.append(ya)
-              plt.plot(x1, y1, ".-g")
-              plt.plot(x2, y2, ".-r")
-              plt.legend(["EKF", "Exact Model"])
-              plt.axis("equal")
-              plt.title( "time %s " %time)
-              plt.grid(True)
+
+          if counter > 1:
+              time = float(line[0])
+              x = float(line[1])
+              y = float(line[2])
+              xpos.append(x)
+              ypos.append(y)
+
+
               filename = "plot_"+str(counter)+".png"
-              plt.savefig(filename)
-              plt.pause(0.001)
-              time += dt
+              #plt.savefig(filename)
+
+              #time += dt
           counter += 1
+
+    plt.plot(xpos, ypos, ".-g")
+    plt.legend(["UKF"])
+    plt.xlabel("X coordinate")
+    plt.ylabel("Y coordinate")
+    plt.axis("equal")
+    #plt.title( "time %s " %time)
+    plt.grid(True)
+    plt.show()
 
 if __name__ == '__main__':
 	main()
 
 	import imageio
 	images = []
-	filenames = [ "plot_"+str(counter+1)+".png" for counter in range(300) if counter % 10 == 0]
+	#filenames = [ "plot_"+str(counter+1)+".png" for counter in range(300) if counter % 10 == 0]
 
-	for filename in filenames:
-		images.append(imageio.imread(filename))
-	imageio.mimsave('movie.gif', images)
+	#for filename in filenames:
+	#	images.append(imageio.imread(filename))
+	#imageio.mimsave('movie.gif', images)
--- cubic_engine/examples/exe27/exe.cpp
+++ cubic_engine/examples/exe27/exe.cpp
@@ -29,14 +29,14 @@ const real_t STD_RADAR_RHO = 0.01;
 const real_t STD_RADAR_THETA = 0.01;
 const real_t STD_RADAR_RHO_D = 0.01;
 
-
 class MotionModel
 {
 public:
 
     typedef DynMat<real_t> matrix_t;
-    typedef SysState<5> state_t;
-    typedef std::tuple<real_t, real_t, std::array<real_t, state_t::dimension>> input_t;
+    typedef SysState<3> state_t;
+    typedef std::tuple<real_t, real_t,
+                       std::array<real_t, state_t::dimension>> input_t;
 
     /// constructor
     MotionModel();
@@ -52,18 +52,16 @@ public:
 private:
 
     /// the state to track
-    SysState<5> state_;
+    SysState<3> state_;
 };
 
 MotionModel::MotionModel()
     :
       state_()
 {
-   state_.set(0, std::make_pair("X", 0.0));
-   state_.set(1, std::make_pair("Y", 0.0));
-   state_.set(2, std::make_pair("Theta", 0.0));
-   state_.set(3, std::make_pair("V", 0.0));
-   state_.set(4, std::make_pair("ThetaDot", 0.0));
+   state_.set(0, {"X", 0.0});
+   state_.set(1, {"Y", 0.0});
+   state_.set(2, {"Theta", 0.0});
 }
 
 DynVec<real_t>
@@ -72,12 +70,25 @@ MotionModel::evaluate(const DynVec<real_t>& vec,
 
     DynVec<real_t> result(vec.size(), 0.0);
     if(std::fabs(std::get<1>(input)) < TOL){
+
        real_t v = std::get<0>(input);
-       result [0] = vec [0] + v*std::cos(state_["Theta"])*DT;
-       result [1] = vec [1] + v*std::sin(state_["Theta"])*DT;
-       result [2] = 0.0;
-       result [3] = 0.0;
-       result [4] = 0.0;
+       auto errors = std::get<2>(input);
+       auto distance = v*DT;
+       auto xincrement = (distance + errors[0])*std::cos(state_["Theta"]  + errors[1]);
+       auto yincrement = (distance + errors[0])*std::sin(state_["Theta"]  + errors[1]);
+
+       result [0] = vec [0] + xincrement;
+       result [1] = vec [1] + yincrement;
+       result [2] = vec [2];
+    }
+    else{
+        real_t v = std::get<0>(input);
+        real_t w = std::get<1>(input);
+        auto errors = std::get<2>(input);
+
+        result[0] = vec[0] + (v/w  + errors[0])*(std::sin(state_["Theta"] + w*DT) - std::sin(state_["Theta"]));
+        result[1] = vec[1] - (v/w  + errors[0])*(-std::cos(state_["Theta"] + w*DT) + std::cos(state_["Theta"]));
+        result[2] = vec[2] + w*DT + errors[1];
 
     }
 
@@ -113,24 +124,23 @@ ObservationModel::evaluate(const std::vector<DynVec<real_t>>& sigma)const{
     for(uint_t sp=0; sp<sigma.size(); ++sp){
 
         auto& vec = result[sp];
-        vec.resize(3, 0.0);
+        vec.resize(2, 0.0);
 
         auto x = sigma[sp][0];
         auto y = sigma[sp][1];
         auto theta = sigma[sp][2];
-        auto v = sigma[sp][3];
 
         vec[0] = std::sqrt(kernel::utils::sqr(x) +
                            kernel::utils::sqr(y));
         vec[1] = theta;
-        vec[2] = (x*std::cos(theta)*v + y*std::sin(theta))/vec[0];
+
     }
 
     return result;
 }
 
 const DynVec<real_t> get_measurement(const SysState<3>& state){
-   return DynVec<real_t>({state.get("X"), state.get("Y")});
+   return DynVec<real_t>({0.0, 0.0});
 }
 
 
@@ -150,7 +160,7 @@ int main() {
     /// linear velocity
     auto vt = 1.0;
 
-    std::array<real_t, 5> motion_control_error;
+    std::array<real_t, MotionModel::state_t::dimension> motion_control_error;
     for(uint_t i=0; i<motion_control_error.size(); ++i){
         motion_control_error[i] = 0.0;
     }
@@ -160,15 +170,13 @@ int main() {
 
     UnscentedKalmanFilter<MotionModel, ObservationModel> ukf(motion_model, observation);
 
-    DynMat<real_t> R(3, 3, 0.0);
+    DynMat<real_t> R(2, 2, 0.0);
     R(0,0) = STD_RADAR_RHO;
     R(1, 1) = STD_RADAR_THETA;
-    R(2, 2) = STD_RADAR_RHO_D;
+
 
     DynMat<real_t> Q(MotionModel::state_t::dimension,
                      MotionModel::state_t::dimension, 0.0);
-    //Q(0,0) = 0.001;
-    //Q(1, 1) = 0.001;
 
     DynMat<real_t> P(MotionModel::state_t::dimension,
                      MotionModel::state_t::dimension, 0.0);
@@ -180,6 +188,7 @@ int main() {
     ukf.set_matrix("P", P);
     ukf.set_matrix("R", R);
     ukf.set_matrix("Q", Q);
+    ukf.initialize_sigma_points(1.0);
 
     kernel::CSVWriter writer("state", kernel::CSVWriter::default_delimiter(), true);
     std::vector<std::string> names{"Time", "X", "Y"};
@@ -192,31 +201,22 @@ int main() {
 
             std::cout<<"At step: "<<step<<" time: "<<time<<std::endl;
 
-            if(counter == 50){
-              w = 0.0*kernel::UnitConverter::degrees_to_rad(45.0);
-            }
-            else if(counter == 100){
-               w = 0.0*kernel::UnitConverter::degrees_to_rad(-45.0);
-            }
-            else if(counter == 150){
-               w = 0.0*kernel::UnitConverter::degrees_to_rad(-45.0);
-            }
-            else{
-                w = 0.0;
-            }
-
-
             auto motion_input = std::make_tuple(vt, w, motion_control_error);
-            //auto& exact_state = exact_motion_model.evaluate(motion_input);
-
             ukf.predict(motion_input);
 
             auto& state = motion_model.get_state();
             auto z = get_measurement(state);
             ukf.update(z);
+            ukf.update_sigma_points();
 
-            std::cout<<"Position: "<<ukf.get("X")<<", "<<ukf.get("Y")<<std::endl;
-            std::cout<<"Orientation: "<<kernel::UnitConverter::rad_to_degrees(ukf.get("Theta"))<<std::endl;
+            std::cout<<"Position: "
+                    <<ukf.get("X")
+                    <<", "
+                    <<ukf.get("Y")
+                    <<std::endl;
+            std::cout<<"Orientation: "
+                    <<kernel::UnitConverter::rad_to_degrees(ukf.get("Theta"))
+                    <<std::endl;
             std::cout<<"V: "<<vt<<", W: "<<w<<std::endl;
 
             std::vector<real_t> row(3, 0.0);
@@ -230,10 +230,12 @@ int main() {
         }
     }
     catch(std::runtime_error& e){
-        std::cerr<<e.what()<<std::endl;
+        std::cerr<<"Runtime error: "
+                 <<e.what()<<std::endl;
     }
     catch(std::logic_error& e){
-        std::cerr<<e.what()<<std::endl;
+        std::cerr<<"Logic error: "
+                 <<e.what()<<std::endl;
     }
     catch(...){
         std::cerr<<"Unknown exception was raised whilst running simulation."<<std::endl;
