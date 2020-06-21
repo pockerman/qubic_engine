d3560bf8abe63baf50fba0bdf50c6b5224693d82
Alexandros Giavaras <a.giavaras@gmail.com>
2020-06-21 18:52:15 +0100

    #224 Update implementation and API

--- cubic_engine/include/cubic_engine/search/rapidly_exploring_random_tree.h
+++ cubic_engine/include/cubic_engine/search/rapidly_exploring_random_tree.h
@@ -12,6 +12,12 @@ namespace search {
 ///
 /// \brief The RRT class models a Rapidly-Exploring Random Tree
 /// see: http://msl.cs.uiuc.edu/~lavalle/papers/Lav98c.pdf
+/// The NodeData most frequently will represent the
+/// state type modeled. It corresponds to the types
+/// of \f$x_{rand}\f$ and \f$x_{new}\f$ from the paper cited above.
+/// The EdgeData type corresponds to the type of \f$u\f$ in the paper.
+/// It is the input that should subsequently be applied to reach from
+/// one state to another and this is what the applications most often will use
 ///
 template<typename NodeData, typename EdgeData>
 class RRT: private boost::noncopyable
@@ -47,14 +53,21 @@ public:
     /// \brief add_vertex Add a new vertex to the tree
     /// \param node The new vertex to add
     ///
-    void add_vertex(const node_t& node);
+    node_t& add_vertex(const node_t& node){ return tree_.add_vertex(node.data);}
+
+    ///
+    /// \brief Add a new vertex in the tree that has the given data
+    ///
+    node_t& add_vertex(const node_data_t& data);
 
     ///
     /// \brief add_edge Add a new edge between the vertices v1 and v2
     /// \param v1 Vertex 1
     /// \param v2 Vertex 2
     ///
-    void add_edge(uint_t v1, uint_t v2);
+    edge_t& add_edge(uint_t v1, uint_t v2){
+        return tree_.add_edge(v1, v2);
+    }
 
     ///
     /// \brief find_nearest_neighbor find the nearest neighbor of other node in this tree
@@ -82,10 +95,12 @@ public:
     ///
     uint_t n_edges()const{return tree_.n_edges();}
 
-    template<typename StateSelector, typename InputSelector, typename DynamicsTp>
+    template<typename StateSelector, typename InputSelector,
+             typename MetricTp, typename DynamicsTp>
     void build(uint_t nitrs, const node_t& xinit,
                const  StateSelector& state_selector,
                const InputSelector& input_selector,
+               const MetricTp& metric,
                DynamicsTp& dynamics);
 
 private:
@@ -103,16 +118,27 @@ RRT<NodeData, EdgeData>::RRT()
       tree_()
 {}
 
+template<typename NodeData, typename EdgeData>
+typename RRT<NodeData, EdgeData>::node_t&
+RRT<NodeData, EdgeData>::add_vertex(const node_data_t& data){
+    return tree_.add_vertex(data);
+}
+
 template<typename NodeTp, typename EdgeTp>
-template<typename StateSelector, typename InputSelector, typename DynamicsTp>
+template<typename StateSelector, typename InputSelector,
+         typename MetricTp, typename DynamicsTp>
 void
 RRT<NodeTp, EdgeTp>::build(uint_t nitrs, const node_t& xinit,
                            const  StateSelector& state_selector,
                            const InputSelector& input_selector,
+                           const MetricTp& metric,
                            DynamicsTp& dynamics){
 
     // initialize the tree
+    auto root = tree_.add_vertex(xinit.data);
 
+    // loop over the states and create
+    // the tree
     for(uint_t itr=0; itr<nitrs; ++itr){
 
         // select a new random state
@@ -120,19 +146,20 @@ RRT<NodeTp, EdgeTp>::build(uint_t nitrs, const node_t& xinit,
 
         // find the nearest neighbor between this tree
         // and the randomly selected state
-        auto xnear = find_nearest_neighbor(xrand);
+        auto xnear = find_nearest_neighbor(xrand, metric);
 
         // select the input
-        auto u = input_selector(xrand, xnear);
+        auto u = input_selector(xrand, xnear.data);
 
         // determine the new state
-        auto xnew = dynamics(xnear, u);
+        auto xnew = dynamics(xnear.data, u);
 
         // add a new vertex
-        add_vertex(xnew);
+        auto new_v = add_vertex(xnew);
 
         // add a new edge
-        add_edge(xnear, xnew, u);
+        auto new_e = add_edge(xnear.id, new_v.id);
+        new_e.set_data(u);
     }
 }
 
@@ -142,15 +169,21 @@ const typename RRT<NodeData, EdgeData>::node_t&
 RRT<NodeData, EdgeData>::find_nearest_neighbor(const node_t& other,
                                                const MetricTp& metric)const{
 
-    // loop over the vertices and apply
-    // the metric
-    auto dist = metric(tree_.get_vertex(0), other);
-    auto result = tree_.get_vertex(0);
+    auto dist = metric(tree_.get_vertex(0).data, other.data);
+    auto result = 0;
 
+    for(uint_t v=1; v< tree_.n_vertices(); ++v){
+        auto vertex_data = tree_.get_vertex(v).data;
 
-    return result;
+        auto new_dist = metric(vertex_data, other.data);
 
+        if(new_dist < dist){
+            dist = new_dist;
+            result = v;
+        }
+    }
 
+    return tree_.get_vertex(result);
 }
 
 }
