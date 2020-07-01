fb91fde33151a477bff01eae106c8b8258c1f232
pockerman <a.giavaras@gmail.com>
2020-07-01 16:03:55 +0100

    Update example

--- cubic_engine/examples/exe30/exe.cpp
+++ cubic_engine/examples/exe30/exe.cpp
@@ -1,5 +1,7 @@
 #include "cubic_engine/base/cubic_engine_types.h"
 #include "kernel/maths/matrix_utilities.h"
+#include "kernel/utilities/data_set_loaders.h"
+#include "kernel/maths/pca.h"
 #include <cmath>
 #include <iostream>
 #include <tuple>
@@ -12,28 +14,24 @@ using cengine::real_t;
 using cengine::DynMat;
 using cengine::DynVec;
 
-}
+void test_case_1(){
 
-int main() {
-   
-    using namespace example;
-    
     DynMat<real_t> X(6, 2);
     X(0,0) = -1.;
     X(0,1) = -1.;
-    
+
     X(1,0) = -2.;
     X(1,1) = -1.;
-    
+
     X(2,0) = -3.;
     X(2,1) = -2.;
-    
+
     X(3,0) = 1.;
     X(3,1) = 1.;
-    
+
     X(4,0) = 2.;
     X(4,1) = 1.;
-    
+
     X(5,0) = 3.;
     X(5,1) = 2.;
 
@@ -56,7 +54,7 @@ int main() {
     DynMat<real_t> U;
     DynVec<real_t> s;
     DynMat<real_t> V;
-    try{
+
 
         std::cout<<"Variable 1 explains: "<<col1_var/total_var<<std::endl;
         std::cout<<"Variable 2 explains: "<<col2_var/total_var<<std::endl;
@@ -95,7 +93,37 @@ int main() {
         std::cout<<"PCA Variable 1 explains: "<<pca_col1_var/pca_total_var<<std::endl;
         std::cout<<"PCA Variable 2 explains: "<<pca_col2_var/pca_total_var<<std::endl;
 
+}
 
+void test_case_2(){
+
+    using  kernel::PCA;
+
+    // load the wine data set
+    auto data = kernel::load_wine_data_set(false);
+
+    // extract the column means
+    auto means = kernel::get_column_means(data.first);
+
+    // keep the first three components
+    // with the largest variance
+    PCA pca(3);
+
+    pca.fit(data.first);
+
+
+
+}
+
+}
+
+int main() {
+   
+    using namespace example;
+    
+    try{
+
+        test_case_1();
     }
     catch(std::runtime_error& e){
         std::cerr<<"Runtime error: "
