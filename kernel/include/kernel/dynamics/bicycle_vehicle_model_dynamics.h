9da033b46d283cc384033aaf8f88a250b3cfc165
pockerman <a.giavaras@gmail.com>
2020-08-15 16:35:09 +0100

    Add bicycle vehicle model dynamics

---
+++ kernel/include/kernel/dynamics/bicycle_vehicle_model_dynamics.h
@@ -0,0 +1,24 @@
+#ifndef BICYCLE_VEHICLE_MODEL_DYNAMICS_H
+#define BICYCLE_VEHICLE_MODEL_DYNAMICS_H
+
+#include "kernel/base/types.h"
+#include "kernel/dynamics/system_state.h"
+#include "kernel/dynamics/motion_model_base.h"
+#include "kernel/dynamics/dynamics_matrix_descriptor.h"
+
+namespace kernel{
+namespace dynamics{
+
+///
+/// \brief The BicycleVehicleModelDynamics class.
+/// Implements the bicycle vehicle model dynamics
+///
+class BicycleVehicleModelDynamics
+{
+
+};
+
+}
+}
+
+#endif // BICYCLE_VEHICLE_MODEL_DYNAMICS_H
