6a3d4894612a4b7129ed5c91c4ff8de823de0519
pockerman <a.giavaras@gmail.com>
2020-08-29 17:14:53 +0100

    Add blaze direct solver

---
+++ kernel/include/kernel/maths/direct_solvers/blaze_direct_solver.h
@@ -0,0 +1,61 @@
+#ifndef BLAZE_DIRECT_SOLVER_H
+#define BLAZE_DIRECT_SOLVER_H
+
+#include "kernel/maths/direct_solvers/direct_solver_base.h"
+#include "kernel/maths/direct_solvers/direct_solver_type.h"
+
+namespace kernel{
+namespace maths {
+namespace solvers {
+
+
+///
+/// \brief The AmesosDirectConfig struct. Helper struct
+/// that wraps parameters to be passed to the AmesosDirect class
+///
+struct BlazeDirectSolverConfig
+{
+
+   DirectSolverType dstype;
+};
+
+class BlazeDirectSolver: public DirectSolverBase<DynMat<real_t>, DynVec<real_t>>
+{
+public:
+
+    ///
+    /// \brief matrix_t The matrix type the solver is using
+    ///
+    typedef typename DirectSolverBase<DynMat<real_t>, DynVec<real_t>>::matrix_t matrix_t;
+
+    ///
+    /// \brief vector_t The vector type the solver is using
+    ///
+    typedef typename DirectSolverBase<DynMat<real_t>, DynVec<real_t>>::vector_t vector_t;
+
+    ///
+    /// \brief BlazeDirectSolver Constructor
+    ///
+    BlazeDirectSolver(const BlazeDirectSolverConfig& config);
+
+    ///
+    /// \brief solve. Solve the system Ax=b
+    ///
+    virtual void solve(matrix_t& A, vector_t& x, vector_t& b )override final;
+
+
+private:
+
+    ///
+    /// \brief config_ Configuration of the solver
+    ///
+    BlazeDirectSolverConfig config_;
+
+};
+
+}
+
+}
+}
+
+#endif // BLAZE_DIRECT_SOLVER_H
