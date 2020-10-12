007c2667297899b19128aba51ba69aba68acfffc
pockerman <a.giavaras@gmail.com>
2020-10-12 21:41:01 +0100

    Update API. Fix compilation warnings

--- cubic_engine/examples/exe12/exe.cpp
+++ cubic_engine/examples/exe12/exe.cpp
@@ -1,11 +1,10 @@
-
 #include "cubic_engine/base/cubic_engine_types.h"
 #include "cubic_engine/ml/supervised_learning/regressor.h"
+#include "cubic_engine/ml/supervised_learning/ordinary_least_squares.h"
 #include "kernel/maths/optimization/serial_gradient_descent.h"
 #include "kernel/maths/optimization/utils/gd_control.h"
-#include "kernel/maths/functions/real_vector_polynomial.h"
-#include "kernel/maths/errorfunctions/mse_function.h"
 #include "kernel/utilities/data_set_loaders.h"
+
 
 #include <iostream>
 
@@ -18,18 +17,28 @@ int main(){
     using kernel::maths::opt::GDConfig;
     using kernel::maths::opt::Gd;
     using cengine::LinearRegression;
+    using cengine::ml::OrdinaryLeastSquares;
 
     try{
 
+        // load the data set
         auto dataset = kernel::load_car_plant_dataset();
+
+        // linear model trained with OLS
+        OrdinaryLeastSquares ols(1, true);
+
+        // train the model
+        ols.train(dataset.first, dataset.second);
+
+        std::cout<<"Intercept: "<<ols.get_interception()<<" slope: "<<ols.coeff(0)<<std::endl;
 
         // The regressor to use. use a hypothesis of the form
         // f = w_0 + w_1*x_1
         // set initial weights to 0
         LinearRegression regressor({0.0, 0.0});
 
-        GDConfig control(10000, kernel::KernelConsts::tolerance(),
-                         GDConfig::DEFAULT_LEARNING_RATE);
+        // configuration for GD
+        GDConfig control(10000, 1.0e-7, GDConfig::DEFAULT_LEARNING_RATE);
 
         Gd gd(control);
 
