8c00c5cd6aeeac3fef1733c90927bf66c059b9e8
Alex <a.giavaras@gmail.com>
2021-03-03 14:53:55 +0000

    #366 365 Add configuration main driver script

---
+++ configure.py
@@ -0,0 +1,17 @@
+import json
+from configuration import INFO
+from configuration.cmake_file_writer import CMakeCubicEngineWriter
+
+
+def read_json(filename):
+
+    with open(filename) as json_file:
+        json_input = json.load(json_file)
+        return json_input
+
+if __name__ == '__main__':
+    print("{0} generating configuration scripts...".format(INFO))
+    config = read_json(filename="config_opts.json")
+    cubic_camke_writer = CMakeCubicEngineWriter(configuration=config)
+    cubic_camke_writer.write_cmake_lists()
+    print("{0} finished...".format(INFO))
