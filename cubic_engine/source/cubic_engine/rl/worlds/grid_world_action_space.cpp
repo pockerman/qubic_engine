4dee9ba13dcd8bcf5146228f50234f7f7ac96f3b
Alex <a.giavaras@gmail.com>
2021-03-12 12:37:14 +0000

    #373 Add uniform sampling from action space

--- cubic_engine/source/cubic_engine/rl/worlds/grid_world_action_space.cpp
+++ cubic_engine/source/cubic_engine/rl/worlds/grid_world_action_space.cpp
@@ -3,6 +3,7 @@
 #ifdef USE_RL
 
 #include "cubic_engine/rl/worlds/grid_world_action_space.h"
+#include <random>
 
 namespace cengine {
 namespace rl {
@@ -23,6 +24,19 @@ std::string to_string(GridWorldAction action){
 
     return "INVALID_ACTION";
 }
+
+GridWorldAction uniform_sample_grid_world_action(){
+
+    // Will be used to obtain a seed for the random number engine
+    std::random_device rd;
+
+    // Standard mersenne_twister_engine seeded with rd()
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 4);
+    auto idx = dis(rd);
+    return static_cast<GridWorldAction>(idx);
+}
+
 }
 }
 }
