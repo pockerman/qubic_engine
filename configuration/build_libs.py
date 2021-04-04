b9b104b7ea06269b3fdb4c912bcb3e6c8ddcbc78
Alex <a.giavaras@gmail.com>
2021-04-04 11:08:56 +0100

    Add build_libs module

---
+++ configuration/build_libs.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+from configuration.utils import remove_directory
+
+
+def build_library(path: Path) -> None:
+
+    if "build" in os.listdir(path=path):
+        remove_directory(path=path, dirname="build")
+        cmd = " cd {0}".format(path.parent)
+        os.system(cmd)
+
+    # that's the library directory
+    str_cmd = "cd {0}".format(path)
+    str_cmd += " && mkdir build && cd build"
+    str_cmd += " && cmake .."
+    str_cmd += " && make "
+    os.system(str_cmd)
+
+    str_cmd = " cd {0}".format(path.parent)
+    os.system(str_cmd)
