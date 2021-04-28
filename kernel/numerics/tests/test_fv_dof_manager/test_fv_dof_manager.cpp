671d666b098e78bd7c450d7cce3c18549b833fd6
Alex <a.giavaras@gmail.com>
2021-04-28 21:24:33 +0100

    Rename test

---
+++ kernel/numerics/tests/test_fv_dof_manager/test_fv_dof_manager.cpp
@@ -0,0 +1,82 @@
+#include "kernel/base/types.h"
+#include "kernel/discretization/dof_object.h"
+
+#include <vector>
+#include <gtest/gtest.h>
+
+namespace{
+
+}
+
+TEST(TestDoFObject, TestDefaultInitialization) {
+
+    /***
+       * Test Scenario:    The application attempts to initialize a default contructed DoFObject
+       * Expected Output:  Object should be constructed
+     **/
+
+    using kernel::numerics::DoFObject;
+    DoFObject dof_obj;
+    ASSERT_EQ(dof_obj.n_dofs(), 0);
+}
+
+
+TEST(TestDotProduct, TestInsertDof) {
+
+    /***
+       * Test Scenario:   The application attempts to insert a valid DoF into DoFObject
+       * Expected Output: DoF should be inserted
+     **/
+
+    using kernel::numerics::DoFObject;
+    using kernel::numerics::DoF;
+    DoFObject dof_obj;
+
+    // make sure we don't have dofs
+    ASSERT_EQ(dof_obj.n_dofs(), 0);
+
+    DoF dof{"dummy_var", 10, true};
+
+    dof_obj.insert_dof(std::move(dof));
+
+    ASSERT_EQ(dof_obj.n_dofs(), 1);
+     ASSERT_EQ(dof_obj.has_variable("dummy_var"), true);
+}
+
+TEST(TestDotProduct, TestReInsertDof) {
+
+    /***
+       * Test Scenario:   The application attempts to insert a  DoF ithat already exists into DoFObject
+       * Expected Output: std::logic_error should be thrown
+     **/
+
+    using kernel::numerics::DoFObject;
+    using kernel::numerics::DoF;
+    DoFObject dof_obj;
+
+    // make sure we don't have dofs
+    ASSERT_EQ(dof_obj.n_dofs(), 0);
+
+    DoF dof{"dummy_var", 10, true};
+
+    dof_obj.insert_dof(std::move(dof));
+
+    ASSERT_EQ(dof_obj.n_dofs(), 1);
+    ASSERT_EQ(dof_obj.has_variable("dummy_var"), true);
+
+    dof = {"dummy_var", 10, true};
+
+    try {
+
+        dof_obj.insert_dof(std::move(dof));
+    }
+    catch (std::logic_error& error) {
+
+        const std::string expected("Dof already exists");
+        ASSERT_EQ(error.what(), expected);
+    }
+}
+
+
+
+
