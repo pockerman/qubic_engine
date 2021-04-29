0e11687b7da6ad2ed05346a269c48d6842a00d37
Alex <a.giavaras@gmail.com>
2021-04-29 11:38:27 +0100

    Add cleaning dirs apps

---
+++ clean_tests_dirs.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import os
+
+from configuration import INFO
+from configuration.utils import remove_directory, remove_file
+
+
+def main(modules):
+    current_dir = Path(os.getcwd())
+
+    for module in modules:
+        module_path = current_dir / module / "tests"
+
+        directories = os.listdir(path=module_path)
+
+        for directory in directories:
+            example_dir = module_path / directory
+            print("{0} cleaning tests {1}".format(INFO, example_dir))
+
+            if "build" in os.listdir(path=example_dir):
+                remove_directory(path=example_dir, dirname="build")
+
+            if "CMakeLists.txt" in os.listdir(path=example_dir):
+                remove_file(path=example_dir, filename="CMakeLists.txt")
+
+
+if __name__ == '__main__':
+    modules = ['kernel/numerics', 'kernel/discretization', 'kernel/kernel']
+    main(modules=modules)
