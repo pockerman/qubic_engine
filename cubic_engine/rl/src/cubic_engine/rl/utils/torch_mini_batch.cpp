809326eb14a441b55bcd7c46c782a945c42e752a
Alex <a.giavaras@gmail.com>
2021-05-28 14:58:31 +0100

    NOP

--- cubic_engine/rl/src/cubic_engine/rl/utils/torch_mini_batch.cpp
+++ cubic_engine/rl/src/cubic_engine/rl/utils/torch_mini_batch.cpp
@@ -7,13 +7,13 @@ namespace utils{
 
 
 MiniBatch<kernel::torch_t>::MiniBatch(torch::Tensor observations,
-          torch::Tensor hidden_states,
-          torch::Tensor actions,
-          torch::Tensor value_predictions,
-          torch::Tensor returns,
-          torch::Tensor masks,
-          torch::Tensor action_log_probs,
-          torch::Tensor advantages)
+                                      torch::Tensor hidden_states,
+                                      torch::Tensor actions,
+                                      torch::Tensor value_predictions,
+                                      torch::Tensor returns,
+                                      torch::Tensor masks,
+                                      torch::Tensor action_log_probs,
+                                      torch::Tensor advantages)
     :
       observations(observations),
       hidden_states(hidden_states),
@@ -22,7 +22,9 @@ MiniBatch<kernel::torch_t>::MiniBatch(torch::Tensor observations,
       returns(returns),
       masks(masks),
       action_log_probs(action_log_probs),
-      advantages(advantages) {}
+      advantages(advantages)
+{}
+
 }
 }
 }
