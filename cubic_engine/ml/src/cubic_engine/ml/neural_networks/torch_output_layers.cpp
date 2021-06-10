835ed3041e0e45437d16950da54cc1d4d6d46557
Alex <a.giavaras@gmail.com>
2021-06-10 08:44:29 +0100

    #401 Update torch layers

--- cubic_engine/ml/src/cubic_engine/ml/neural_networks/torch_output_layers.cpp
+++ cubic_engine/ml/src/cubic_engine/ml/neural_networks/torch_output_layers.cpp
@@ -2,22 +2,22 @@
 
 #ifdef USE_PYTORCH
 
-#include "kernel/numerics/statistics/torch_normal.h"
-#include "kernel/numerics/statistics/torch_bernoulli.h"
-#include "kernel/numerics/statistics/torch_categorical.h"
+#include "cubic_engine/ml/statistics/torch_normal.h"
+#include "cubic_engine/ml/statistics/torch_bernoulli.h"
+#include "cubic_engine/ml/statistics/torch_categorical.h"
 
 namespace cengine {
 namespace ml {
 namespace nets {
 
-LinearOutputLayer::LinearOutputLayer(uint_t num_inputs, uint_t num_outputs)
+LinearOutputLayer::LinearOutputLayer(unsigned int num_inputs, unsigned int num_outputs)
     :
       linear(num_inputs, num_outputs)
 {
     register_module("linear", linear);
 }
 
-BernoulliOutput::BernoulliOutput(uint_t num_inputs, uint_t num_outputs)
+BernoulliOutput::BernoulliOutput(unsigned int num_inputs, unsigned int num_outputs)
     :
     LinearOutputLayer(num_inputs, num_outputs)
 {
@@ -26,11 +26,11 @@ BernoulliOutput::BernoulliOutput(uint_t num_inputs, uint_t num_outputs)
     //torch::init_weights(linear->named_parameters(), 0.01, 0);
 }
 
-std::unique_ptr<kernel::numerics::stats::TorchDistribution>
+std::unique_ptr<ml::stats::TorchDistribution>
 BernoulliOutput::forward(torch::Tensor x){
 
     x = linear(x);
-    return std::make_unique<kernel::numerics::stats::TorchBernoulli>(nullptr, &x);
+    return std::make_unique<ml::stats::TorchBernoulli>(nullptr, &x);
 
 }
 
@@ -43,11 +43,11 @@ CategoricalOutput::CategoricalOutput(uint_t num_inputs, uint_t num_outputs)
 }
 
 
-std::unique_ptr<kernel::numerics::stats::TorchDistribution>
+std::unique_ptr<ml::stats::TorchDistribution>
 CategoricalOutput::forward(torch::Tensor x){
 
     x = linear(x);
-    return std::make_unique<kernel::numerics::stats::TorchCategorical>(nullptr, &x);
+    return std::make_unique<ml::stats::TorchCategorical>(nullptr, &x);
 
 }
 
@@ -63,12 +63,12 @@ NormalOutput::NormalOutput(uint_t num_inputs, uint_t num_outputs)
 }
 
 
-std::unique_ptr<kernel::numerics::stats::TorchDistribution>
+std::unique_ptr<ml::stats::TorchDistribution>
 NormalOutput::forward(torch::Tensor x){
 
     auto loc = linear(x);
     auto scale = scale_log.exp();
-    return std::make_unique<kernel::numerics::stats::TorchNormalDistribution>(loc, scale);
+    return std::make_unique<ml::stats::TorchNormalDistribution>(loc, scale);
 
 }
 
