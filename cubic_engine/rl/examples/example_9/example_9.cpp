4ae672474dd2fa9b4e244be491a74488b8e32210
Alex <a.giavaras@gmail.com>
2021-06-17 10:16:27 +0100

    Update example 9

--- cubic_engine/rl/examples/example_9/example_9.cpp
+++ cubic_engine/rl/examples/example_9/example_9.cpp
@@ -5,10 +5,10 @@
 #endif
 
 #include "cubic_engine/base/cubic_engine_types.h"
-#include "cubic_engine/rl/worlds/discrete_gym_world.h"
 #include "cubic_engine/rl/gym_comm/communicator.h"
-#include "cubic_engine/rl/q_learning.h"
-#include "torch/torch.h"
+#include "cubic_engine/rl/worlds/gym_lunar_lander_world.h"
+
+//#include "torch/torch.h"
 
 #include <iostream>
 #include <memory>
@@ -18,15 +18,12 @@ namespace example{
 
 
 // Environment parameters
-const std::string env_name = "CartPole-v0";
 const int num_envs = 1;
 const std::string server_addr = "tcp://127.0.0.1:10201";
 
 using cengine::uint_t;
 using cengine::rl::gym::Communicator;
-using cengine::rl::worlds::DiscreteGymWorld;
-using cengine::rl::QLearning;
-using cengine::rl::QLearningInput;
+using cengine::rl::worlds::GymLunarLanderWorld;
 
 }
 
@@ -45,15 +42,10 @@ int main(){
         // create the communicator
         Communicator communicator(server_addr);
 
-        DiscreteGymWorld<uint_t> world(communicator);
-        world.build(env_name);
+        GymLunarLanderWorld environment("v2", communicator);
+        environment.build(true);
 
-        std::cout<<"Number of states="<<world.n_states()<<std::endl;
-        std::cout<<"Number of actions="<<world.n_actions()<<std::endl;
-
-        QLearningInput qinput;
-        QLearning<DiscreteGymWorld<uint_t>> qlearner(qinput);
-        qlearner.initialize(world, 0.0);
+        std::cout<<"Environment name: "<<environment.name()<<std::endl;
 
     }
     catch(std::exception& e){
