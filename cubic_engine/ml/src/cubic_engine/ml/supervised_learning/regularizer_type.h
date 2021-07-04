90ae4b45a018017c5ab84c0cbbcb8a29e4f5f9a8
Alex <a.giavaras@gmail.com>
2021-07-04 13:15:57 +0100

    Add regularizer types

---
+++ cubic_engine/ml/src/cubic_engine/ml/supervised_learning/regularizer_type.h
@@ -0,0 +1,16 @@
+#ifndef REGULARIZER_TYPE_H
+#define REGULARIZER_TYPE_H
+
+namespace cengine {
+namespace ml {
+
+///
+/// \brief The RegularizerType enum
+///
+enum class RegularizerType{LASSO, RIDGE, INVALID_TYPE};
+
+}
+
+}
+
+#endif // REGULARIZER_TYPE_H
