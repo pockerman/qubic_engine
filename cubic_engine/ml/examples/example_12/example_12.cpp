106db6e294f64e7570ed8ab1b5afc80247bdfe63
Alex <a.giavaras@gmail.com>
2021-07-11 19:59:34 +0100

    Add example_12

---
+++ cubic_engine/ml/examples/example_12/example_12.cpp
@@ -0,0 +1,48 @@
+#include "cubic_engine/base/cubic_engine_types.h"
+#include "cubic_engine/ml/supervised_learning/linear_regressor.h"
+#include "cubic_engine/ml/supervised_learning/regularizer_type.h"
+#include "kernel/utilities/data_set_loaders.h"
+
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <any>
+
+int main(){
+
+    using cengine::uint_t;
+    using cengine::real_t;
+    using cengine::DynMat;
+    using cengine::DynVec;
+
+    using cengine::ml::LinearRegressor;
+
+    try{
+
+        // load the data set
+        auto dataset = kernel::load_car_plant_dataset();
+
+        // linear model trained with OLS
+        LinearRegressor regressor(1, true, cengine::ml::RegularizerType::NONE);
+
+        // train the model
+        std::map<std::string, std::any> options;
+        regressor.fit(dataset, options);
+
+        std::cout<<"Intercept: "<<regressor.get_interception()<<" slope: "<<regressor.get_parameters()[1]<<std::endl;
+
+
+    }
+    catch(std::exception& e){
+
+        std::cerr<<e.what()<<std::endl;
+    }
+    catch(...){
+
+        std::cerr<<"Unknown exception occured"<<std::endl;
+    }
+
+    return 0;
+}
+
