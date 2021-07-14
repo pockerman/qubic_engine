73aff456dd7fa39f787684432306c8b12e1d7d8b
Alex <a.giavaras@gmail.com>
2021-07-14 12:35:36 +0100

    Add TimeStep class

---
+++ cubic_engine/rl/src/cubic_engine/rl/worlds/time_step.h
@@ -0,0 +1,21 @@
+#ifndef TIME_STEP_H
+#define TIME_STEP_H
+
+namespace cengine{
+namespace rl {
+namespace envs {
+
+///
+/// \brief The TimeStep class. Helper class that
+/// models the result of acting on an environment
+///
+class TimeStep{
+
+};
+
+}
+
+}
+}
+
+#endif // TIME_STEP_H
