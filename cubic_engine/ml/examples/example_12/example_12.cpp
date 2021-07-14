d6b5f1d5dfeadc50f6be50e702bc99335ecb04d9
Alex <a.giavaras@gmail.com>
2021-07-14 14:10:14 +0100

    Fix example

--- cubic_engine/ml/examples/example_12/example_12.cpp
+++ cubic_engine/ml/examples/example_12/example_12.cpp
@@ -1,7 +1,8 @@
 #include "cubic_engine/base/cubic_engine_types.h"
 #include "cubic_engine/ml/supervised_learning/linear_regressor.h"
 #include "cubic_engine/ml/supervised_learning/regularizer_type.h"
-#include "kernel/utilities/data_set_loaders.h"
+#include "cubic_engine/ml/datasets/blaze_regression_dataset.h"
+#include "cubic_engine/ml/datasets/data_set_loaders.h"
 
 
 #include <iostream>
@@ -15,13 +16,14 @@ int main(){
     using cengine::real_t;
     using cengine::DynMat;
     using cengine::DynVec;
-
+    using cengine::ml::BlazeRegressionDataset;
     using cengine::ml::LinearRegressor;
 
     try{
 
         // load the data set
-        auto dataset = kernel::load_car_plant_dataset();
+        BlazeRegressionDataset dataset;
+        //auto dataset = kernel::load_car_plant_dataset();
 
         // linear model trained with OLS
         LinearRegressor regressor(1, true, cengine::ml::RegularizerType::NONE);
