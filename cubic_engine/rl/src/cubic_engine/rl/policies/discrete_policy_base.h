a8c221da35efd29fd9aed4dc79c8c55cc3f79fc1
Alex <a.giavaras@gmail.com>
2021-07-22 09:04:27 +0100

    Add policies

---
+++ cubic_engine/rl/src/cubic_engine/rl/policies/discrete_policy_base.h
@@ -0,0 +1,62 @@
+#ifndef DISCRETE_POLICY_BASE_H
+#define DISCRETE_POLICY_BASE_H
+
+#include "cubic_engine/base/cubic_engine_types.h"
+#include "cubic_engine/rl/policies/policy_base.h"
+
+#include <vector>
+#include <utility>
+
+namespace cengine {
+namespace rl {
+namespace policies {
+
+class DiscretePolicyBase: public PolicyBase
+{
+public:
+
+    ///
+    /// \brief Destructor
+    ///
+    virtual ~DiscretePolicyBase() = default;
+
+
+    ///
+    /// \brief operator []
+    ///
+    virtual std::vector<std::pair<uint_t, real_t>> operator[](uint_t sidx)const = 0;
+
+    ///
+    /// \brief equals
+    ///
+    virtual bool equals(const DiscretePolicyBase& other)const = 0;
+
+
+protected:
+
+    ///
+    /// \brief DiscretePolicyBase
+    /// \param n_states
+    /// \param n_actions
+    ///
+    DiscretePolicyBase() = default;
+
+};
+
+inline
+bool operator==(const DiscretePolicyBase& p1, const DiscretePolicyBase& p2){
+    return p1.equals(p2);
+}
+
+inline
+bool operator !=(const DiscretePolicyBase& p1, const DiscretePolicyBase& p2){
+    return !(p1 == p2);
+}
+
+}
+
+}
+
+}
+
+#endif // DISCRETE_POLICY_BASE_H
---
+++ cubic_engine/rl/src/cubic_engine/rl/policies/policy_base.h
@@ -0,0 +1,31 @@
+#ifndef POLICY_BASE_H
+#define POLICY_BASE_H
+
+namespace cengine {
+namespace rl {
+namespace policies {
+
+class PolicyBase
+{
+public:
+
+    ///
+    ///
+    ///
+    virtual ~PolicyBase() = default;
+
+protected:
+
+    ///
+    ///
+    ///
+    PolicyBase() = default;
+};
+
+
+
+}
+}
+}
+
+#endif // POLICY_BASE_H
