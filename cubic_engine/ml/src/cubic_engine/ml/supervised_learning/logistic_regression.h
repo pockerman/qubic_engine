b3fb98e893baae53ba1984e1226b12d0346e5d9d
Alex <a.giavaras@gmail.com>
2021-08-01 12:53:36 +0100

    Add score function

--- cubic_engine/ml/src/cubic_engine/ml/supervised_learning/logistic_regression.h
+++ cubic_engine/ml/src/cubic_engine/ml/supervised_learning/logistic_regression.h
@@ -54,6 +54,12 @@ class LogisticRegression: public  LinearParametricModel<uint_t>
     ///
     std::vector<value_t> predict_many(const DynMat<real_t>& data)const{}
 
+    ///
+    /// \brief score
+    ///
+    template<typename DataSetTp>
+    real_t score(const DataSetTp& data)const;
+
 private:
 
 
@@ -65,11 +71,10 @@ private:
 
 template<typename DataSetTp, typename SolverTp>
 typename SolverTp::output_t
-LogisticRegression::fit(const DataSetTp& dataset, SolverTp& solver, const std::map<std::string, std::any>& options){
+LogisticRegression::fit(const DataSetTp& dataset, SolverTp& solver, const std::map<std::string, std::any>& /*options*/){
 
 #ifdef KERNEL_DEBUG
     assert(dataset.n_features() == polynomial_.n_coeffs() && "Invalid feature space size");
-    //check_options_(options);
 #endif
 
     auto err_type = kernel::ErrorFuncType::MSE;
@@ -78,6 +83,31 @@ LogisticRegression::fit(const DataSetTp& dataset, SolverTp& solver, const std::m
     auto output = solver.solve(dataset.feature_matrix(), dataset.labels(), *error_function_ptr.get());
 
     return output;
+}
+
+template<typename DataSetTp>
+real_t
+LogisticRegression::score(const DataSetTp& dataset)const{
+
+#ifdef KERNEL_DEBUG
+    assert(dataset.n_features() == polynomial_.n_coeffs() && "Invalid feature space size");
+    //check_options_(options);
+#endif
+
+    real_t correctly_clss = 0;
+    for(uint_t p=0; p < dataset.size(); ++p){
+
+        auto[point, label] = dataset[p];
+
+        auto p_class = static_cast<uint_t>(predict_one(point));
+
+        if(p_class == label){
+            correctly_clss += 1.;
+        }
+    }
+
+    return correctly_clss/dataset.size();
+
 }
  
 
