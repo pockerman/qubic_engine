5250bd1f7d22b34fd6425e56c12e36341b5ba6fe
Alex <a.giavaras@gmail.com>
2021-08-03 16:59:59 +0100

    Finish example

---
+++ cubic_engine/rl/examples/example_7/plot.py
@@ -0,0 +1,44 @@
+"""
+Utility script for plotting with matplotlib
+"""
+import matplotlib.pyplot as plt
+import csv
+import math
+import numpy as np
+
+
+def plot_values(v):
+    # reshape value function
+    V_sq = np.reshape(v, (4, 4))
+
+    # plot the state-value function
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111)
+    im = ax.imshow(V_sq, cmap='cool')
+    for (j, i), label in np.ndenumerate(V_sq):
+        ax.text(i, j, np.round(label, 5), ha='center', va='center', fontsize=14)
+    plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+    plt.title('State-Value Function')
+    plt.show()
+
+def main(filename):
+	
+	with open(filename, 'r', newline='') as csvfile:
+		csv_file_reader = csv.reader(csvfile, delimiter=",")
+		
+		value_func = []
+		for row in csv_file_reader:
+			if not row[0].startswith('#'):
+				try:
+					value_func.append(float(row[1]))
+				except:
+					continue
+		plot_values(np.array(value_func))
+       
+		
+if __name__ == '__main__':
+  main("policy_iteration.csv")
+	
+	
+
+
