e8f4a763b6a26a661b8a7b622b7a478a20dace28
Alex <a.giavaras@gmail.com>
2021-08-12 08:35:50 +0100

    Update API

--- cubic_engine/rl/src/cubic_engine/rl/policies/epsilon_greedy_policy.h
+++ cubic_engine/rl/src/cubic_engine/rl/policies/epsilon_greedy_policy.h
@@ -21,14 +21,20 @@ public:
     /// \param min_eps
     /// \param max_eps
     ///
-    EpsilonGreedyPolicy(real_t eps, bool reduce=false,
-                        real_t min_eps = 0.01, real_t max_eps=1.0 );
+    explicit EpsilonGreedyPolicy(real_t eps, uint_t n_actions, uint_t seed=0,
+                                 bool reduce=false, real_t min_eps = 0.01, real_t max_eps=1.0 );
 
     ///
     ///
     ///
     template<typename QMapTp>
-    uint_t operator()(const QMapTp& q_map)const;
+    uint_t operator()(const QMapTp& q_map, uint_t state)const;
+
+    ///
+    /// \brief adjust_on_episode
+    /// \param episode
+    ///
+    void adjust_on_episode(uint_t episode);
 
 private:
 
@@ -36,24 +42,49 @@ private:
     real_t eps_;
     real_t min_eps_;
     real_t max_eps_;
+    uint_t n_actions_;
+    uint_t seed_;
     bool reduce_eps_;
 
+    mutable std::uniform_real_distribution<> real_dist_;
+    mutable std::uniform_int_distribution<> distrib_;
 };
 
-EpsilonGreedyPolicy::EpsilonGreedyPolicy(real_t eps, bool reduce,
-                                         real_t min_eps, real_t max_eps)
+EpsilonGreedyPolicy::EpsilonGreedyPolicy(real_t eps,  uint_t n_actions, uint_t seed,
+                                         bool reduce, real_t min_eps, real_t max_eps)
     :
       eps_init_(eps),
       eps_(eps),
       min_eps_(min_eps),
       max_eps_(max_eps),
-      reduce_eps_(reduce)
+      n_actions_(n_actions_),
+      seed_(seed),
+      reduce_eps_(reduce),
+      real_dist_(0.0, 1.0),
+      distrib_(0,  n_actions_ - 1)
 {}
 
 template<typename QMapTp>
 uint_t
-EpsilonGreedyPolicy::operator()(const QMapTp& q_map)const{
+EpsilonGreedyPolicy::operator()(const QMapTp& q_map, uint_t state)const{
 
+
+    std::mt19937 gen(seed_);
+
+
+    if(real_dist_(gen) > eps_){
+        // select greedy action with probability epsilon
+        return blaze::max(q_map[state]);
+    }
+
+    return distrib_(gen);
+}
+
+void
+EpsilonGreedyPolicy::adjust_on_episode(uint_t episode){
+    if(reduce_eps_){
+        eps_ /= eps_ / episode;
+    }
 }
 
 }
