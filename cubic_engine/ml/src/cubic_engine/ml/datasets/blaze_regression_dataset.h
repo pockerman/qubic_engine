2fd582ef2a0f3f7f73ecd07139f6829c0b48c469
Alex <a.giavaras@gmail.com>
2021-08-15 10:56:21 +0100

    Change API

--- cubic_engine/ml/src/cubic_engine/ml/datasets/blaze_regression_dataset.h
+++ cubic_engine/ml/src/cubic_engine/ml/datasets/blaze_regression_dataset.h
@@ -3,6 +3,7 @@
 
 #include "cubic_engine/base/cubic_engine_types.h"
 #include "cubic_engine/ml/datasets/regression_dataset_base.h"
+#include "cubic_engine/ml/datasets/dataset_point_type.h"
 
 #include <boost/noncopyable.hpp>
 #include <string>
@@ -10,6 +11,8 @@
 
 namespace cengine {
 namespace ml{
+
+
 
 ///
 /// \brief The BlazeRegressionDataset class
@@ -143,6 +146,17 @@ public:
     ///
     auto columns()const-> const std::map<std::string, uint_t>&{return columns_;}
 
+    ///
+    ///
+    ///
+    template<typename OperatorTp>
+    DynVec<real_t> accumulate(const OperatorTp& op, uint_t limit, PointTp type=PointTp::START)const;
+
+    ///
+    ///
+    ///
+    template<typename OpTp>
+    void iterate(const OpTp& op)const;
 
 
 private:
@@ -163,6 +177,21 @@ private:
     ///
     std::map<std::string, uint_t> columns_;
 
+
+    ///
+    ///
+    ///
+    template<typename OperatorTp>
+    friend DynVec<real_t> accumulate_head(const BlazeRegressionDataset& dataset, const OperatorTp& op,
+                                    uint_t limit);
+
+    ///
+    ///
+    ///
+    template<typename OperatorTp>
+    friend DynVec<real_t> accumulate_tail(const BlazeRegressionDataset& dataset, const OperatorTp& op,
+                                    uint_t limit);
+
 };
 
 
@@ -177,6 +206,65 @@ void
 BlazeRegressionDataset::load_from_file(FileReader& reader){
     reader.read(examples_, labels_, columns());
 }
+
+template<typename OperatorTp>
+DynVec<real_t>
+accumulate_head(const BlazeRegressionDataset& dataset, const OperatorTp& op, uint_t limit){
+
+    if(dataset.empty()){
+        return DynVec<real_t>();
+    }
+
+    DynVec<real_t> result(limit, 0.0);
+
+    for(uint_t i=0; i < limit; ++i){
+        auto[row, label] = dataset[i];
+        result[i] = op(row, label);
+    }
+    return result;
+}
+
+template<typename OperatorTp>
+DynVec<real_t>
+accumulate_tail(const BlazeRegressionDataset& dataset, const OperatorTp& op, uint_t limit){
+
+    if(dataset.empty()){
+        return DynVec<real_t>();
+    }
+
+    DynVec<real_t> result(limit, 0.0);
+    auto start = dataset.n_rows() - 1;
+    auto end = dataset.n_rows() - limit;
+    for(uint_t i=0; start< end;  --start, i++){
+        auto[row, label] = dataset[start];
+        result[i] = op(row, label);
+    }
+    return result;
+}
+
+
+template<typename OperatorTp>
+DynVec<real_t>
+BlazeRegressionDataset::accumulate(const OperatorTp& op, uint_t limit, PointTp type)const{
+
+    if(type == PointTp::END){
+        return accumulate_tail<OperatorTp>(*this, op, limit);
+    }
+
+     return accumulate_head<OperatorTp>(*this, op, limit);
+}
+
+template<typename OpTp>
+void
+BlazeRegressionDataset::iterate(const OpTp& op)const{
+
+    for(uint_t i=0; i < n_rows(); ++i){
+        auto [row, label] = (*this)[i];
+        op(row, label);
+    }
+}
+
+
 
 }
 }
