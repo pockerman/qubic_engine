6ccfdbd3aef0d4ce38fa476100f67669869a3257
Alex <a.giavaras@gmail.com>
2021-08-21 21:29:42 +0100

    Update policy

--- cubic_engine/rl/src/cubic_engine/rl/policies/epsilon_greedy_policy.h
+++ cubic_engine/rl/src/cubic_engine/rl/policies/epsilon_greedy_policy.h
@@ -2,10 +2,15 @@
 #define EPSILON_GREEDY_POLICY_H
 
 #include "cubic_engine/base/cubic_engine_types.h"
+#include <random>
+#include <cmath>
 
 namespace cengine {
 namespace rl {
 namespace policies {
+
+
+enum class EpsilonDecayOption{NONE, EXPONENTIAL, INVERSE_STEP, CONSTANT_RATE};
 
 ///
 /// \brief The EpsilonGreedyPolicy class
@@ -21,8 +26,8 @@ public:
     /// \param min_eps
     /// \param max_eps
     ///
-    explicit EpsilonGreedyPolicy(real_t eps, uint_t n_actions, uint_t seed=0,
-                                 bool reduce=false, real_t min_eps = 0.01, real_t max_eps=1.0 );
+    explicit EpsilonGreedyPolicy(real_t eps, uint_t n_actions, EpsilonDecayOption decay_op,
+                                 real_t min_eps = 0.01, real_t max_eps=1.0,  uint_t seed=0 );
 
     ///
     ///
@@ -36,54 +41,96 @@ public:
     ///
     void adjust_on_episode(uint_t episode);
 
+    ///
+    /// \brief reset
+    ///
+    void reset(){eps_ = eps_init_;}
+
+    ///
+    /// \brief set_epsilon_decay_factor
+    /// \param eps_decay
+    ///
+    void set_epsilon_decay_factor(real_t eps_decay){epsilon_decay_ = eps_decay;}
+
 private:
 
     real_t eps_init_;
     real_t eps_;
     real_t min_eps_;
     real_t max_eps_;
+    real_t epsilon_decay_;
     uint_t n_actions_;
     uint_t seed_;
-    bool reduce_eps_;
+    EpsilonDecayOption decay_op_;
 
-    mutable std::uniform_real_distribution<> real_dist_;
-    mutable std::uniform_int_distribution<> distrib_;
+    //mutable std::uniform_real_distribution<> real_dist_;
+    //mutable std::uniform_int_distribution<> distrib_;
 };
 
-EpsilonGreedyPolicy::EpsilonGreedyPolicy(real_t eps,  uint_t n_actions, uint_t seed,
-                                         bool reduce, real_t min_eps, real_t max_eps)
+EpsilonGreedyPolicy::EpsilonGreedyPolicy(real_t eps, uint_t n_actions, EpsilonDecayOption decay_op,
+                                         real_t min_eps, real_t max_eps,  uint_t seed)
     :
       eps_init_(eps),
       eps_(eps),
       min_eps_(min_eps),
       max_eps_(max_eps),
-      n_actions_(n_actions_),
+      epsilon_decay_(0.01),
+      n_actions_(n_actions),
       seed_(seed),
-      reduce_eps_(reduce),
-      real_dist_(0.0, 1.0),
-      distrib_(0,  n_actions_ - 1)
+      decay_op_(decay_op)
 {}
 
 template<typename QMapTp>
 uint_t
 EpsilonGreedyPolicy::operator()(const QMapTp& q_map, uint_t state)const{
 
+    const auto& actions = q_map.find(state)->second;
 
-    std::mt19937 gen(seed_);
-
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> real_dist_(0.0, 1.0);
 
     if(real_dist_(gen) > eps_){
         // select greedy action with probability epsilon
-        return blaze::max(q_map[state]);
+        return blaze::argmax(actions);
     }
 
+    //std::mt19937 another_gen(seed_);
+    std::uniform_int_distribution<> distrib_(0,  n_actions_ - 1);
     return distrib_(gen);
 }
 
 void
 EpsilonGreedyPolicy::adjust_on_episode(uint_t episode){
-    if(reduce_eps_){
-        eps_ /= eps_ / episode;
+
+    if(decay_op_ == EpsilonDecayOption::NONE)
+        return;
+
+    if(decay_op_  == EpsilonDecayOption::INVERSE_STEP ){
+
+        if(episode == 0){
+            episode = 1;
+        }
+
+        // there are various methods to do epsilon
+        // reduction
+        eps_ = 1.0 / episode;
+
+        if(eps_ < min_eps_){
+            eps_ = min_eps_;
+        }
+    }
+    else if(decay_op_ == EpsilonDecayOption::EXPONENTIAL){
+        eps_ = min_eps_ + (max_eps_ - min_eps_)*std::exp(-epsilon_decay_ * episode);
+    }
+    else if(decay_op_ == EpsilonDecayOption::CONSTANT_RATE){
+
+        eps_ -= epsilon_decay_;
+
+        if(eps_ < min_eps_){
+            eps_ = min_eps_;
+        }
+
     }
 }
 
